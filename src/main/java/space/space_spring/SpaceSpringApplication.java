d2758d310715e181427b6379d988ed966d8063e5
seongjunnoh <joon1754@naver.com>
2024-07-07 14:26:02 +0900

    feat 스프링 프로젝트 생성

---
+++ src/main/java/space/space_spring/SpaceSpringApplication.java
@@ -0,0 +1,13 @@
+package space.space_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceSpringApplication.class, args);
+	}
+
+}

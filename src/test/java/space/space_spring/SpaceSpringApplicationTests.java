c55a24fa359c42f1f9993c96823679d7b7aef8c6
seongjunnoh <joon1754@naver.com>
2024-07-07 14:26:02 +0900

    feat 스프링 프로젝트 생성

---
+++ src/test/java/space/space_spring/SpaceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package space.space_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

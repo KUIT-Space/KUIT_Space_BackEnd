42bad1d992078efc90de24dd1cab5d8d34bcbe21
seongjunnoh <joon1754@naver.com>
2024-07-10 11:26:10 +0900

    test 배포 자동화 테스트

--- src/main/java/space/space_spring/Controller/TestController.java
+++ src/main/java/space/space_spring/Controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
+    @GetMapping("/test1")
     public String test(){
         return "CI/CD 환경 구축 테스트 중. 이 메세지가 보인다면 성공입니다";
     }

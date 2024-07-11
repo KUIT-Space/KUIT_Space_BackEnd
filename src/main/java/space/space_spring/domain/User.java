1596b9b6d53e52617b407194a69c20516db01024
seongjunnoh <joon1754@naver.com>
2024-07-11 16:27:57 +0900

    Refactor build 에러 수정 테스트

--- src/main/java/space/space_spring/domain/User.java
+++ src/main/java/space/space_spring/domain/User.java
@@ -1,11 +1,9 @@
 package space.space_spring.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "Users")
 public class User extends BaseEntity {
 
     @Id @GeneratedValue

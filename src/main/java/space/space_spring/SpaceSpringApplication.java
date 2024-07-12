9ec079e622ff5f0fdb74f99fffb56aa594b65118
seongjunnoh <joon1754@naver.com>
2024-07-12 17:19:33 +0900

    Test 유저회원 가입 테스트

--- src/main/java/space/space_spring/SpaceSpringApplication.java
+++ src/main/java/space/space_spring/SpaceSpringApplication.java
@@ -2,6 +2,7 @@ package space.space_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 public class SpaceSpringApplication {
--- src/main/java/space/space_spring/dao/UserDao.java
+++ src/main/java/space/space_spring/dao/UserDao.java
@@ -2,13 +2,13 @@ package space.space_spring.dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import space.space_spring.domain.User;
 import space.space_spring.dto.PostUserRequest;
 
 @Repository
-@Transactional
 public class UserDao {
 
     @PersistenceContext
@@ -20,5 +20,11 @@ public class UserDao {
 
         em.persist(user);
         return user.getUserId();
+    }
+
+    public User findUserByEmail(String email) {
+        TypedQuery<User> query = em.createQuery("SELECT u FROM User u WHERE u.email = :email", User.class);
+        query.setParameter("email", email);
+        return query.getSingleResult();
     }
 }

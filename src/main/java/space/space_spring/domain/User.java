f09760dcaf93b3f48b46be912f8d101b72cb5caf
seongjunnoh <joon1754@naver.com>
2024-07-13 16:39:18 +0900

    Refactor user 회원가입 시 validation을 User -> PostUserRequest로 변경

--- src/main/java/space/space_spring/domain/User.java
+++ src/main/java/space/space_spring/domain/User.java
@@ -17,18 +17,12 @@ public class User extends BaseEntity {
     private Long userId;
 
     @Column(name = "email")
-    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")     // 이메일 정규표현식 확인 필요함
     private String email;
 
     @Column(name = "password")
-    @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$",
-            message = "8~16글자의 영문 대/소문자, 숫자, 특수문자가 포함되어야 합니다."
-    )
     private String password;
 
     @Column(name = "user_name")
-    @Length(min = 1, max = 10, message = "이름은 10자이내의 문자열이어야 합니다.")
     private String userName;
 
     @Column(name = "jwt_token")

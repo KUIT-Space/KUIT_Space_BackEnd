450732390368689510d730f91223ee28d2fdb4c8
seongjunnoh <joon1754@naver.com>
2024-07-14 11:44:46 +0900

    refactor testController url 수정

--- src/main/java/space/space_spring/controller/TestController.java
+++ src/main/java/space/space_spring/controller/TestController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/test1")
+    @GetMapping("/test")
     public String test(){
         return "CI/CD 환경 구축 테스트 중. 이 메세지가 보인다면 성공입니다";
-//        파이팅~~~
     }
 }

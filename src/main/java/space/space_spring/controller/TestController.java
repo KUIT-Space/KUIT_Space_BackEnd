e794032cbcea03866383f392d3d9dbbf265398b9
seongjunnoh <joon1754@naver.com>
2024-07-15 11:57:21 +0900

    Feat jwt 관련 exception, interceptor 추가

--- src/main/java/space/space_spring/controller/TestController.java
+++ src/main/java/space/space_spring/controller/TestController.java
@@ -2,6 +2,7 @@ package space.space_spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import space.space_spring.response.BaseResponse;
 
 @RestController
 public class TestController {
@@ -9,5 +10,10 @@ public class TestController {
     @GetMapping("/test")
     public String test(){
         return "CI/CD 환경 구축 테스트 중. 이 메세지가 보인다면 성공입니다";
+    }
+
+    @GetMapping("/test/jwt")
+    public BaseResponse<String> jwtTest() {
+        return new BaseResponse<>("jwt test 성공");
     }
 }

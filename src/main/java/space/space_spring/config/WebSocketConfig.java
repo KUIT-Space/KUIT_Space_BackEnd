b05306e9b883ed3623f9319adc558ef57ecef2c7
hyunn522 <tjgus52213@gmail.com>
2024-07-17 20:34:26 +0900

    feat: WebSocketConfig 파일 작성

---
+++ src/main/java/space/space_spring/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package space.space_spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // stomp 접속 주소 url = ws://localhost:8080/ws
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*");
+//                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 메세지 구독(수신) 요청의 엔드포인트
+        registry.enableSimpleBroker("/sub");
+
+        // 메세지 발행(송신) 요청의 엔드 포인트
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+}

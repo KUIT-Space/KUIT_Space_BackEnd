a58e57f21a05f7179d637534617a52133bd10baa
hyunn522 <tjgus52213@gmail.com>
2024-07-19 16:19:29 +0900

    feat: cors 설정에 web local address 추가

--- src/main/java/space/space_spring/config/WebConfig.java
+++ src/main/java/space/space_spring/config/WebConfig.java
@@ -3,6 +3,7 @@ package space.space_spring.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import space.space_spring.argument_resolver.jwtLogin.JwtLoginAuthHandlerArgumentResolver;
@@ -22,6 +23,8 @@ public class WebConfig implements WebMvcConfigurer {
     private final JwtLoginAuthHandlerArgumentResolver jwtLoginAuthHandlerArgumentResolver;
     private final JwtUserSpaceAuthHandlerArgumentResolver jwtUserSpaceAuthHandlerArgumentResolver;
 
+    private static final String DEVELOP_FRONT_ADDRESS = "http://localhost:3000";
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtLoginAuthInterceptor)
@@ -38,4 +41,15 @@ public class WebConfig implements WebMvcConfigurer {
         argumentResolvers.add(jwtLoginAuthHandlerArgumentResolver);
         argumentResolvers.add(jwtUserSpaceAuthHandlerArgumentResolver);
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(DEVELOP_FRONT_ADDRESS)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .exposedHeaders("location")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
 }

70d024b6501e50e31f5448b58ccb2f2fac272c7e
hyunn522 <tjgus52213@gmail.com>
2024-07-19 16:22:42 +0900

    feat: 소켓 연결 및 메세지 전송 테스트

--- src/main/java/space/space_spring/controller/TestController.java
+++ src/main/java/space/space_spring/controller/TestController.java
@@ -1,10 +1,17 @@
 package space.space_spring.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import space.space_spring.argument_resolver.jwtLogin.JwtLoginAuth;
 import space.space_spring.argument_resolver.jwtUserSpace.JwtUserSpaceAuth;
+import space.space_spring.dto.chat.ChatTestRequest;
+import space.space_spring.dto.chat.ChatTestResponse;
 import space.space_spring.dto.jwt.JwtPayloadDto;
 import space.space_spring.response.BaseResponse;
 
@@ -27,5 +34,17 @@ public class TestController {
     public BaseResponse<String> jwtUserSpaceTest(@JwtUserSpaceAuth JwtPayloadDto jwtPayloadDto) {
         log.info("jwtPayloadDto = {}", jwtPayloadDto);
         return new BaseResponse<>("jwt user space test 성공");
+    }
+
+    @MessageMapping("/chat/{spaceChatId}") // {spaceChatId} 채팅방으로 보낸 메세지 매핑
+    @SendTo("/topic/chat/{spaceChatId}") // {spaceChatId} 채팅방을 구독한 곳들로 메세지 전송
+    public ChatTestResponse webSocketTest(@Payload ChatTestRequest chat, @DestinationVariable String spaceChatId) {
+        log.info(spaceChatId + " 채팅방으로 " + chat.getMsg() + " 전송");
+        return ChatTestResponse.of(chat.getMsg());
+    }
+
+    @SubscribeMapping("/topic/chat/{spaceChatId}") // {spaceChatId} 채팅방을 구독
+    public void webSocketTest(@DestinationVariable String spaceChatId) {
+        log.info(spaceChatId + " 채팅방 구독");
     }
 }

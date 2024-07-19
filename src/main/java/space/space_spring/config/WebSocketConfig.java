7339f6e3887753ad3e9ede1e0f3b4ade60a79d22
hyunn522 <tjgus52213@gmail.com>
2024-07-19 16:19:48 +0900

    feat: end point 변경

--- src/main/java/space/space_spring/config/WebSocketConfig.java
+++ src/main/java/space/space_spring/config/WebSocketConfig.java
@@ -20,9 +20,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // 메세지 구독(수신) 요청의 엔드포인트
-        registry.enableSimpleBroker("/sub");
+        registry.enableSimpleBroker("/topic");
 
         // 메세지 발행(송신) 요청의 엔드 포인트
-        registry.setApplicationDestinationPrefixes("/pub");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 }

2c5377faf69a8062a61fc4e7e57d7fecc289b1d1
seongjunnoh <joon1754@naver.com>
2024-07-20 11:27:08 +0900

    Refactor jwtUserSpace 관련 코드 삭제

--- src/main/java/space/space_spring/controller/SpaceController.java
+++ src/main/java/space/space_spring/controller/SpaceController.java
@@ -9,12 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import space.space_spring.argument_resolver.jwtLogin.JwtLoginAuth;
-import space.space_spring.argument_resolver.jwtUserSpace.JwtUserSpaceAuth;
-import space.space_spring.dto.jwt.JwtPayloadDto;
 import space.space_spring.dto.space.PostSpaceCreateRequest;
 import space.space_spring.dto.space.PostSpaceCreateResponse;
 
-import space.space_spring.dto.space.SpaceCreateDto;
 import space.space_spring.exception.SpaceException;
 import space.space_spring.response.BaseResponse;
 import space.space_spring.service.SpaceService;
@@ -30,16 +27,12 @@ public class SpaceController {
     private final SpaceService spaceService;
 
     @PostMapping("/create")
-    public BaseResponse<PostSpaceCreateResponse> createSpace(@JwtLoginAuth Long userId, @Validated @RequestBody PostSpaceCreateRequest postSpaceCreateRequest, BindingResult bindingResult, HttpServletResponse response) {
+    public BaseResponse<PostSpaceCreateResponse> createSpace(@JwtLoginAuth Long userId, @Validated @RequestBody PostSpaceCreateRequest postSpaceCreateRequest, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             throw new SpaceException(INVALID_SPACE_CREATE, getErrorMessage(bindingResult));
         }
 
-        SpaceCreateDto spaceCreateDto = spaceService.createSpace(userId, postSpaceCreateRequest);
-        String jwtUserSpace = spaceCreateDto.getJwtUserSpace();
-        response.setHeader("Authorization", "Bearer " + jwtUserSpace);
-
-        return new BaseResponse<>(new PostSpaceCreateResponse(spaceCreateDto.getSpaceId()));
+        return new BaseResponse<>(spaceService.createSpace(userId, postSpaceCreateRequest));
     }
 
 }

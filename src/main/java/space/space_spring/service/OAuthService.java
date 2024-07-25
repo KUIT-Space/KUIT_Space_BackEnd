be80c0bf92c6534219f34f3c9786114f8d42239a
seongjunnoh <joon1754@naver.com>
2024-07-25 11:37:36 +0900

    Feat 카카오 서버에서 받은 access token 으로 유저 정보 get

--- src/main/java/space/space_spring/service/OAuthService.java
+++ src/main/java/space/space_spring/service/OAuthService.java
@@ -11,19 +11,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-import space.space_spring.dto.oauthDto.KakaoLoginDto;
-import space.space_spring.dto.oauthInfo.KakaoInfo;
+import space.space_spring.dto.oAuthInfo.KakaoInfo;
 
 @Service
 public class OAuthService {
 
+    /**
+     * 카카오 인증 서버가 전달해준 유저의 인가코드로 토큰 발급 요청
+     */
     public String getAccessToken(String code, String clientId, String redirectUri, String clientSecret) throws JsonProcessingException {
 
-        // HTTP Header 생성
+        // TODO 1. HTTP Header 생성
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
-        // HTTP Body 생성
+        // TODO 2. HTTP Body 생성
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", clientId);
@@ -31,7 +33,7 @@ public class OAuthService {
         body.add("code", code);
         body.add("client_secret", clientSecret);
 
-        // HTTP 요청 보내기
+        // TODO 3. 카카오 인증 서버로 HTTP 요청 보내기
         HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(body, headers);
         RestTemplate rt = new RestTemplate();
         ResponseEntity<String> response = rt.exchange(
@@ -41,7 +43,7 @@ public class OAuthService {
                 String.class
         );
 
-        // HTTP 응답 (JSON) -> 액세스 토큰 파싱
+        // TODO 4. 카카오 인증 서버로부터의 HTTP 응답 (JSON) -> 액세스 토큰만 파싱
         String responseBody = response.getBody();
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readTree(responseBody);
@@ -49,13 +51,17 @@ public class OAuthService {
         return jsonNode.get("access_token").asText();
     }
 
+    /**
+     * 카카오 인증 서버로부터 받은 access token 으로 해당 유저의 사용자 정보 가져오기
+     */
     public KakaoInfo getKakaoInfo(String accessToken) throws JsonProcessingException {
-        // HTTP Header 생성
+
+        // TODO 1. HTTP Header 생성
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Bearer " + accessToken);
         headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
-        // HTTP 요청 보내기
+        // TODO 2. 카카오 api 서버로 HTTP 요청 보내기
         HttpEntity<MultiValueMap<String, String>> kakaoUserInfoRequest = new HttpEntity<>(headers);
         RestTemplate rt = new RestTemplate();
         ResponseEntity<String> response = rt.exchange(
@@ -65,20 +71,16 @@ public class OAuthService {
                 String.class
         );
 
-        // responseBody에 있는 정보 꺼내기
+        // TODO 3. responseBody에 있는 정보 꺼내기
         String responseBody = response.getBody();
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readTree(responseBody);
 
-        Long id = jsonNode.get("id").asLong();
+        // 유저의 이메일, nickname 정보 get
         String email = jsonNode.get("kakao_account").get("email").asText();
-        String nickname = jsonNode.get("properties")
-                .get("nickname").asText();
+        String nickname = jsonNode.get("properties").get("nickname").asText();
 
-        return new KakaoInfo(id, nickname, email);
+        return new KakaoInfo(nickname, email);
     }
 
-//    public KakaoLoginDto ifNeedKakaoInfo(KakaoInfo kakaoInfo) {
-//
-//    }
 }

5533375adc37c9b31aa46568da8c42b253abfc6e
arkchive <kyungminkim7@konkuk.ac.kr>
2024-07-31 04:40:11 +0900

    feat: 게시판 BoardService 게시글 조회 로직 추가

--- src/main/java/space/space_spring/service/BoardService.java
+++ src/main/java/space/space_spring/service/BoardService.java
@@ -3,11 +3,51 @@ package space.space_spring.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import space.space_spring.dao.BoardDao;
+import space.space_spring.dao.UserDao;
+import space.space_spring.dto.post.ReadBoardResponse;
+import space.space_spring.entity.Board;
+import space.space_spring.entity.Space;
+import space.space_spring.entity.User;
+import space.space_spring.util.space.SpaceUtils;
+import space.space_spring.util.user.UserUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
+
+    private final UserUtils userUtils;
+    private final SpaceUtils spaceUtils;
     private final BoardDao boardDao;
+
+    public List<ReadBoardResponse> getAllPosts(Long userId, Long spaceId) {
+        // TODO 1: userId에 해당하는 user find
+        User userByUserId = userUtils.findUserByUserId(userId);
+
+        // TODO 2: spaceId에 해당하는 space find
+        Space spaceBySpaceId = spaceUtils.findSpaceBySpaceId(spaceId);
+
+        // TODO 3: 해당 user의 해당 space 내의 게시판 게시글 리스트 return
+        List<Board> posts = boardDao.findByUserAndSpace(userByUserId, spaceBySpaceId);
+        return posts.stream()
+                .map(this::convertToReadResponse)
+                .collect(Collectors.toList());
+    }
+
+    public ReadBoardResponse convertToReadResponse(Board board) {
+        return ReadBoardResponse.builder()
+                .postId(board.getPostId())
+                .user(board.getUser())
+                .space(board.getSpace())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .type(board.getType())
+                .like(board.getLike())
+                .build();
+
+    }
 
 
 }

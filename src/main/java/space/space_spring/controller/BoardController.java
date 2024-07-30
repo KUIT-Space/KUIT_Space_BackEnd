2a5e0507ac84845ef7c6a9d7459f5d34044c854a
arkchive <kyungminkim7@konkuk.ac.kr>
2024-07-31 04:40:28 +0900

    feat: 게시판 BoardController 게시글 조회 로직 추가

--- src/main/java/space/space_spring/controller/BoardController.java
+++ src/main/java/space/space_spring/controller/BoardController.java
@@ -2,15 +2,28 @@ package space.space_spring.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import space.space_spring.argument_resolver.jwtLogin.JwtLoginAuth;
+import space.space_spring.dto.post.ReadBoardResponse;
+import space.space_spring.response.BaseResponse;
 import space.space_spring.service.BoardService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/space/{spaceId}/board")
+@RequestMapping("/space/{spaceId}")
 @Slf4j
 public class BoardController {
-    private final BoardService postService;
+    private final BoardService boardService;
+
+    @GetMapping("/board")
+    public BaseResponse<List<ReadBoardResponse>> getBoard(@JwtLoginAuth Long userId, @PathVariable Long spaceId) {
+        List<ReadBoardResponse> board = boardService.getAllPosts(userId, spaceId);
+        return new BaseResponse<>(board);
+    }
 
 }

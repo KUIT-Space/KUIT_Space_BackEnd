19a18463c3529b958e1a78896202cf1840386c7d
arkchive <kyungminkim7@konkuk.ac.kr>
2024-07-31 04:39:36 +0900

    feat: 게시판 BoardDao 로직 추가

--- src/main/java/space/space_spring/dao/BoardDao.java
+++ src/main/java/space/space_spring/dao/BoardDao.java
@@ -1,12 +1,16 @@
 package space.space_spring.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import space.space_spring.entity.Board;
+import space.space_spring.entity.Space;
+import space.space_spring.entity.User;
+
+import java.util.List;
 
 @Repository
-public class BoardDao {
+public interface BoardDao extends JpaRepository<Board, Long> {
 
-
-
-
+    List<Board> findByUserAndSpace(User user, Space space);
 
 }

4505edf32811d6d701dc68f1e9a749cdec26e09c
seongjunnoh <joon1754@naver.com>
2024-07-31 16:02:58 +0900

    refactor : 로컬 로그인 api response dto 삭제

--- src/main/java/space/space_spring/controller/UserController.java
+++ src/main/java/space/space_spring/controller/UserController.java
@@ -36,7 +36,7 @@ public class UserController {
             throw new UserException(INVALID_USER_SIGNUP, getErrorMessage(bindingResult));
         }
 
-        Long signup = userService.signup(postUserSignupRequest);
+        userService.signup(postUserSignupRequest);
 
         return new BaseResponse<>("로컬 회원가입 성공");
     }
@@ -45,14 +45,15 @@ public class UserController {
      * 로그인
      */
     @PostMapping("/login")
-    public BaseResponse<PostUserLoginResponse> login(@Validated @RequestBody PostUserLoginRequest postUserLoginRequest, BindingResult bindingResult, HttpServletResponse response) {
+    public BaseResponse<String> login(@Validated @RequestBody PostUserLoginRequest postUserLoginRequest, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             throw new UserException(INVALID_USER_LOGIN, getErrorMessage(bindingResult));
         }
 
         String jwtLogin = userService.login(postUserLoginRequest);
         response.setHeader("Authorization", "Bearer " + jwtLogin);
-        return new BaseResponse<>(new PostUserLoginResponse("로그인 성공"));
+
+        return new BaseResponse<>("로컬 로그인 성공");
     }
 
     /**

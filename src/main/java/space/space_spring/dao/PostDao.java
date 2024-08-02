1940a9a53ad0496325eb75508c75b76d191c858e
arkchive <kyungminkim7@konkuk.ac.kr>
2024-08-02 11:06:46 +0900

    feat: board->post 이름 수정

--- src/main/java/space/space_spring/dao/PostDao.java
+++ src/main/java/space/space_spring/dao/PostDao.java
@@ -2,15 +2,14 @@ package space.space_spring.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import space.space_spring.entity.Board;
+import space.space_spring.entity.Post;
 import space.space_spring.entity.Space;
-import space.space_spring.entity.User;
 
 import java.util.List;
 
 @Repository
-public interface BoardDao extends JpaRepository<Board, Long> {
+public interface PostDao extends JpaRepository<Post, Long> {
 
-    List<Board> findBySpace(Space space);
+    List<Post> findBySpace(Space space);
 
 }
---
+++ src/main/java/space/space_spring/service/PostService.java
@@ -0,0 +1,46 @@
+package space.space_spring.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import space.space_spring.dao.PostDao;
+import space.space_spring.dto.post.ReadPostsResponse;
+import space.space_spring.entity.Post;
+import space.space_spring.entity.Space;
+import space.space_spring.util.space.SpaceUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final SpaceUtils spaceUtils;
+    private final PostDao postDao;
+
+    public List<ReadPostsResponse> getAllPosts(Long spaceId) {
+
+        // TODO 1: spaceId에 해당하는 space find
+        Space spaceBySpaceId = spaceUtils.findSpaceBySpaceId(spaceId);
+
+        // TODO 2: 해당 user의 해당 space 내의 게시판 게시글 리스트 return
+        List<Post> posts = postDao.findBySpace(spaceBySpaceId);
+        return posts.stream()
+                .map(this::convertToReadResponse)
+                .collect(Collectors.toList());
+    }
+
+    public ReadPostsResponse convertToReadResponse(Post post) {
+        return ReadPostsResponse.builder()
+                .postId(post.getPostId())
+                .spaceId(post.getSpace().getSpaceId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .type(post.getType())
+                .like_count(post.getLike())
+                .build();
+
+    }
+
+
+}

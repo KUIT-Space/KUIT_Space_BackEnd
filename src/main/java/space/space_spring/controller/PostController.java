65b4ef2efac7f56cbe10051b98f12b82301b206d
arkchive <kyungminkim7@konkuk.ac.kr>
2024-08-06 04:49:42 +0900

    feat: isUserInSpace 검증 로직 추가

--- src/main/java/space/space_spring/controller/PostController.java
+++ src/main/java/space/space_spring/controller/PostController.java
@@ -3,11 +3,15 @@ package space.space_spring.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import space.space_spring.argument_resolver.jwtLogin.JwtLoginAuth;
 import space.space_spring.dto.post.ReadPostsResponse;
+import space.space_spring.entity.UserSpace;
 import space.space_spring.response.BaseResponse;
 import space.space_spring.service.PostService;
+import space.space_spring.util.userSpace.UserSpaceUtils;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,11 +19,15 @@ import java.util.List;
 @Slf4j
 public class PostController {
     private final PostService postService;
+    private final UserSpaceUtils userSpaceUtils;
 
     @GetMapping("/board")
     public BaseResponse<List<ReadPostsResponse>> getBoard(
+            @JwtLoginAuth Long userId,
             @PathVariable Long spaceId,
             @RequestParam(defaultValue = "all") String filter) {
+        Optional<UserSpace> userInSpace = userSpaceUtils.isUserInSpace(userId, spaceId);
+        log.info("UserName = {}, UserSpaceAuth = {}", userInSpace.get().getUserName(), userInSpace.get().getUserSpaceAuth());
         List<ReadPostsResponse> board = postService.getAllPosts(spaceId, filter);
         return new BaseResponse<>(board);
     }

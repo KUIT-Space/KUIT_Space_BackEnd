bbce9208a1480baf596ce0306c8859adab97b0a2
hyunn522 <tjgus52213@gmail.com>
2024-08-07 21:28:23 +0900

    feat: service단에 전송된 채팅 저장 로직 추가

---
+++ src/main/java/space/space_spring/service/ChattingService.java
@@ -0,0 +1,62 @@
+package space.space_spring.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import space.space_spring.dao.SpaceDao;
+import space.space_spring.dao.UserSpaceDao;
+import space.space_spring.dao.chat.ChattingDao;
+import space.space_spring.dto.chat.request.ChatMessageRequest;
+import space.space_spring.dto.chat.response.ChatMessageResponse;
+import space.space_spring.entity.Space;
+import space.space_spring.entity.User;
+import space.space_spring.entity.UserSpace;
+import space.space_spring.entity.document.ChatMessage;
+import space.space_spring.exception.UserSpaceException;
+import space.space_spring.util.user.UserUtils;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
+import static space.space_spring.response.status.BaseExceptionResponseStatus.USER_IS_NOT_IN_SPACE;
+
+@Service
+@RequiredArgsConstructor
+public class ChattingService {
+
+    private final UserUtils userUtils;
+    private final SpaceDao spaceDao;
+    private final UserSpaceDao userSpaceDao;
+    private final ChattingDao chattingDao;
+
+    @Transactional
+    public ChatMessageResponse sendChatMessage(ChatMessageRequest chatMessageRequest, Long chatRoomId) {
+
+        // TODO 1: 메시지 메타데이터 저장 위해 전송자 찾기
+        User sender = userUtils.findUserByUserId(chatMessageRequest.getSenderId());
+        Space space = spaceDao.findSpaceBySpaceId(chatMessageRequest.getSpaceId());
+        Optional<UserSpace> senderInSpace = Optional.ofNullable(userSpaceDao.findUserSpaceByUserAndSpace(sender, space)
+                .orElseThrow(() -> new UserSpaceException(USER_IS_NOT_IN_SPACE)));
+
+        // TODO 2: validation 후 전송자 프로필 사진 get
+        String senderProfileImg = "";
+        if (senderInSpace.isPresent()) {
+            senderProfileImg = senderInSpace.get().getUserProfileImg();
+        }
+
+        // TODO 3: DB에 메시지 저장
+        ChatMessage message = chattingDao.insert(ChatMessage.of(
+                chatMessageRequest.getContent(),
+                chatRoomId,
+                chatMessageRequest.getSpaceId(),
+                sender.getUserId(),
+                sender.getUserName(),
+                senderProfileImg,
+                chatMessageRequest.getMessageType(),
+                ZonedDateTime.now(ZoneId.of("Asia/Seoul")).toLocalDateTime()
+        ));
+
+        return ChatMessageResponse.of(message);
+    }
+}

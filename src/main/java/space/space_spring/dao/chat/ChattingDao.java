3aaa165546c33e02ebbd17c10664e7268ae4e0f4
hyunn522 <tjgus52213@gmail.com>
2024-08-09 17:14:45 +0900

    feat: 채팅 로그 dto 및 dao 추가

--- src/main/java/space/space_spring/dao/chat/ChattingDao.java
+++ src/main/java/space/space_spring/dao/chat/ChattingDao.java
@@ -4,6 +4,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import space.space_spring.entity.document.ChatMessage;
 
+import java.util.List;
+
 @Repository
 public interface ChattingDao extends MongoRepository<ChatMessage, String> {
+    List<ChatMessage> findByChatRoomId(Long chatRoomId);
 }

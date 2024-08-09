fdb562db83da10473a8528d763b96ed6c0e867f3
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2024-08-09 19:50:38 +0900

    [feat] Custom Exception handler

---
+++ src/main/java/space/space_spring/exception_handler/JwtExceptionControllerAdvice.java
@@ -0,0 +1,31 @@
+package space.space_spring.exception_handler;
+
+import jakarta.annotation.Priority;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import space.space_spring.exception.jwt.bad_request.JwtBadRequestException;
+import space.space_spring.exception.jwt.unauthorized.JwtUnauthorizedTokenException;
+import space.space_spring.response.BaseErrorResponse;
+
+@Slf4j
+@Priority(0)
+@RestControllerAdvice
+public class JwtExceptionControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(JwtBadRequestException.class)
+    public BaseErrorResponse handle_JwtBadRequestException(JwtBadRequestException e) {
+        log.error("[handle_JwtBadRequestException]", e);
+        return new BaseErrorResponse(e.getExceptionStatus());
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(JwtUnauthorizedTokenException.class)
+    public BaseErrorResponse handle_JwtUnauthorizedException(JwtUnauthorizedTokenException e) {
+        log.error("[handle_JwtUnauthorizedException]", e);
+        return new BaseErrorResponse(e.getExceptionStatus());
+    }
+}

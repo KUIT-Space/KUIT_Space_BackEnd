600b987f5c7e4f60059d53bfa88508ac3aa4719b
hyunn522 <tjgus52213@gmail.com>
2024-08-09 22:56:20 +0900

    feat: 유저가 읽은 마지막 시간 저장 로직 추가

---
+++ src/main/java/space/space_spring/service/UserChatRoomService.java
@@ -0,0 +1,38 @@
+package space.space_spring.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import space.space_spring.dao.chat.ChatRoomDao;
+import space.space_spring.dao.chat.UserChatRoomDao;
+import space.space_spring.entity.ChatRoom;
+import space.space_spring.entity.User;
+import space.space_spring.entity.UserChatRoom;
+import space.space_spring.util.user.UserUtils;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserChatRoomService {
+
+    private final UserUtils userUtils;
+
+    private final ChatRoomDao chatRoomDao;
+    private final UserChatRoomDao userChatRoomDao;
+
+    @Transactional
+    public void saveLastReadTime(Long userId, Long chatRoomId) {
+        User userByUserId = userUtils.findUserByUserId(userId);
+        Optional<ChatRoom> chatRoomByChatRoomId = chatRoomDao.findById(chatRoomId);
+
+        chatRoomByChatRoomId.ifPresent(chatRoom -> {
+            UserChatRoom targetChatRoom = userChatRoomDao.findByUserAndChatRoom(userByUserId, chatRoom);
+            targetChatRoom.setLastReadTime(LocalDateTime.now(ZoneId.of("Asia/Seoul")));
+        });
+    }
+}

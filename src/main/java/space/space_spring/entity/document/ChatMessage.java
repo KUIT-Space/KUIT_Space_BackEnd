4fd573ee4c8161ab93d81f3323f2fd8236e08ef7
hyunn522 <tjgus52213@gmail.com>
2024-08-10 18:01:01 +0900

    refactor: 정적 팩토리 메소드 리팩토링 및 불필요한 코드 제거

--- src/main/java/space/space_spring/entity/document/ChatMessage.java
+++ src/main/java/space/space_spring/entity/document/ChatMessage.java
@@ -5,9 +5,11 @@ import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
+import space.space_spring.dto.chat.request.ChatMessageRequest;
 import space.space_spring.entity.enumStatus.ChatMessageType;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.HashMap;
 
 @Document(collection = "chat_message")
@@ -34,17 +36,16 @@ public class ChatMessage {
 
     private LocalDateTime createdAt;
 
-    public static ChatMessage of(HashMap<String, String> content, Long chatRoomId, Long spaceId, Long senderId, String senderName, String senderImg, ChatMessageType messageType, LocalDateTime createdAt) {
+    public static ChatMessage of(ChatMessageRequest chatMessageRequest, Long chatRoomId, Long senderId, String senderName, String senderImg) {
         return ChatMessage.builder()
-                .content(content)
+                .content(chatMessageRequest.getContent())
                 .chatRoomId(chatRoomId)
-                .spaceId(spaceId)
+                .spaceId(chatMessageRequest.getSpaceId())
                 .senderId(senderId)
                 .senderName(senderName)
                 .senderImg(senderImg)
-                .messageType(messageType)
-                .createdAt(createdAt)
+                .messageType(chatMessageRequest.getMessageType())
+                .createdAt(LocalDateTime.now(ZoneId.of("Asia/Seoul")))
                 .build();
     }
-
 }

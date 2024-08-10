34482b13720fa904b338d7daa060e809c89ab837
hyunn522 <tjgus52213@gmail.com>
2024-08-10 17:56:33 +0900

    refactor: utils 메소드 사용

--- src/main/java/space/space_spring/service/ChattingService.java
+++ src/main/java/space/space_spring/service/ChattingService.java
@@ -3,20 +3,13 @@ package space.space_spring.service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import space.space_spring.dao.SpaceDao;
-import space.space_spring.dao.UserSpaceDao;
-import space.space_spring.dao.chat.ChatRoomDao;
 import space.space_spring.dao.chat.ChattingDao;
 import space.space_spring.dto.chat.request.ChatMessageRequest;
 import space.space_spring.dto.chat.response.ChatMessageLogResponse;
 import space.space_spring.dto.chat.response.ChatMessageResponse;
-import space.space_spring.entity.ChatRoom;
-import space.space_spring.entity.Space;
-import space.space_spring.entity.User;
 import space.space_spring.entity.UserSpace;
 import space.space_spring.entity.document.ChatMessage;
-import space.space_spring.exception.UserSpaceException;
-import space.space_spring.util.user.UserUtils;
+import space.space_spring.util.userSpace.UserSpaceUtils;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -24,33 +17,27 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import static space.space_spring.response.status.BaseExceptionResponseStatus.USER_IS_NOT_IN_SPACE;
 
 @Service
 @RequiredArgsConstructor
 public class ChattingService {
 
-    private final UserUtils userUtils;
-    private final SpaceDao spaceDao;
-    private final UserSpaceDao userSpaceDao;
+    private final UserSpaceUtils userSpaceUtils;
     private final ChattingDao chattingDao;
-    private final ChatRoomDao chatRoomDao;
 
     @Transactional
     public ChatMessageResponse sendChatMessage(Long senderId, ChatMessageRequest chatMessageRequest, Long chatRoomId) {
 
         // TODO 1: 메시지 메타데이터 저장 위해 전송자 찾기
-        User sender = userUtils.findUserByUserId(senderId);
-        Space space = spaceDao.findSpaceBySpaceId(chatMessageRequest.getSpaceId());
-        Optional<UserSpace> senderInSpace = Optional.ofNullable(userSpaceDao.findUserSpaceByUserAndSpace(sender, space)
-                .orElseThrow(() -> new UserSpaceException(USER_IS_NOT_IN_SPACE)));
+        Optional<UserSpace> senderInSpace = userSpaceUtils.isUserInSpace(senderId, chatMessageRequest.getSpaceId());
 
-        // TODO 2: validation 후 전송자 프로필 사진 get
+        // TODO 2: validation 후 전송자 이름 및 프로필 사진 get
+        String senderName = "";
         String senderProfileImg = "";
         if (senderInSpace.isPresent()) {
+            senderName = senderInSpace.get().getUserName();
             senderProfileImg = senderInSpace.get().getUserProfileImg();
         }
-
 
         // TODO 3: DB에 메시지 저장
         ChatMessage message = chattingDao.insert(ChatMessage.of(
@@ -58,7 +45,7 @@ public class ChattingService {
                 chatRoomId,
                 chatMessageRequest.getSpaceId(),
                 senderId,
-                sender.getUserName(),
+                senderName,
                 senderProfileImg,
                 chatMessageRequest.getMessageType(),
                 LocalDateTime.now(ZoneId.of("Asia/Seoul"))

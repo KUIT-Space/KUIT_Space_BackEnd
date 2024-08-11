13a87b093468270dec8cfe4186a4649f08a9c475
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2024-08-12 02:58:47 +0900

    [Feat] devlop @CheckUserSpace

--- src/main/java/space/space_spring/interceptor/UserSpaceValidationInterceptor.java
+++ src/main/java/space/space_spring/interceptor/UserSpaceValidationInterceptor.java
@@ -4,8 +4,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.HandlerMapping;
+import space.space_spring.argumentResolver.userSpace.CheckUserSpace;
 import space.space_spring.dao.SpaceDao;
 import space.space_spring.dao.UserDao;
 import space.space_spring.dao.UserSpaceDao;
@@ -27,8 +29,14 @@ public class UserSpaceValidationInterceptor implements HandlerInterceptor {
     private final UserDao userDao;
     private final SpaceDao spaceDao;
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,Object Handler) throws Exception{
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,Object handler) throws Exception{
 
+        // @CheckUserSpace(require=false)인 경우 검증하지 않음
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        CheckUserSpace methodAnnotation= handlerMethod.getMethodAnnotation(CheckUserSpace.class);
+        if(methodAnnotation!=null && !methodAnnotation.required()){
+            return true;
+        }
         Long userId = (Long) request.getAttribute("userId");
 
         // URL에서 spaceId 추출

0886b711a139bb6170ee8061cf96561533f0e260
hyunn522 <tjgus52213@gmail.com>
2024-08-14 22:12:46 +0900

    feat: 메시지 타입에 따라 파일 처리 로직 추가

--- src/main/java/space/space_spring/controller/ChattingController.java
+++ src/main/java/space/space_spring/controller/ChattingController.java
@@ -14,6 +14,7 @@ import space.space_spring.dto.chat.response.ChatMessageResponse;
 import space.space_spring.service.ChattingService;
 import space.space_spring.service.UserChatRoomService;
 
+import java.io.IOException;
 import java.util.Map;
 
 @Slf4j
@@ -28,7 +29,7 @@ public class ChattingController {
     @MessageMapping("/chat/{chatRoomId}") // {chatRoomId} 채팅방으로 보낸 메세지 매핑
     @SendTo("/topic/chat/{chatRoomId}") // {chatRoomId} 채팅방을 구독한 곳들로 메세지 전송
     public ChatMessageResponse sendChatMessage (@Payload ChatMessageRequest chatMessageRequest, @DestinationVariable Long chatRoomId,
-                                                @Header("simpSessionAttributes") Map<String, Object> sessionAttributes) {
+                                                @Header("simpSessionAttributes") Map<String, Object> sessionAttributes) throws IOException {
         Long senderId = (Long) sessionAttributes.get("userId");
 //        log.info(senderId + " 님이 " + chatRoomId + " 채팅방으로 " + chatMessageRequest.getContent() + " 전송");
 

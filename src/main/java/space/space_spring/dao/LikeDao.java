6ceff45fc6f637e01fb8d2924c01a5e50945b070
arkchive <kyungminkim7@konkuk.ac.kr>
2024-08-15 18:22:09 +0900

    feat: 댓글 관련 좋아요 로직 생성 및 게시글 관련 좋아요 로직 수정

--- src/main/java/space/space_spring/dao/LikeDao.java
+++ src/main/java/space/space_spring/dao/LikeDao.java
@@ -1,12 +1,12 @@
 package space.space_spring.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import space.space_spring.entity.Post;
-import space.space_spring.entity.PostLike;
-import space.space_spring.entity.User;
+import space.space_spring.entity.*;
 
 import java.util.Optional;
 
 public interface LikeDao extends JpaRepository<PostLike, Long> {
     Optional<PostLike> findByUserAndPost(User user, Post post);
+
+    Optional<CommentLike> findByUserAndComment(User user, Comment comment);
 }

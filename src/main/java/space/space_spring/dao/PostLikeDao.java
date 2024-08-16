72d862b3f5925dc4256de844e72eb435a81052e5
arkchive <kyungminkim7@konkuk.ac.kr>
2024-08-16 18:03:09 +0900

    feat: LikeDao 분리

--- src/main/java/space/space_spring/dao/PostLikeDao.java
+++ src/main/java/space/space_spring/dao/PostLikeDao.java
@@ -5,8 +5,6 @@ import space.space_spring.entity.*;
 
 import java.util.Optional;
 
-public interface LikeDao extends JpaRepository<PostLike, Long> {
+public interface PostLikeDao extends JpaRepository<PostLike, Long> {
     Optional<PostLike> findByUserAndPost(User user, Post post);
-
-    Optional<CommentLike> findByUserAndComment(User user, Comment comment);
 }

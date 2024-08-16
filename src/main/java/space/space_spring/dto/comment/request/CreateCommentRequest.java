0b21e972bd620742f2ecb7d3eb4ce9a5e769370a
arkchive <kyungminkim7@konkuk.ac.kr>
2024-08-16 18:45:15 +0900

    refactor: 댓글 생성 시 댓글 아이디 반환되도록 리팩토링

--- src/main/java/space/space_spring/dto/comment/request/CreateCommentRequest.java
+++ src/main/java/space/space_spring/dto/comment/request/CreateCommentRequest.java
@@ -1,28 +1,43 @@
 package space.space_spring.dto.comment.request;
 
+import com.mongodb.lang.Nullable;
 import lombok.*;
 import space.space_spring.entity.Comment;
 import space.space_spring.entity.Post;
 import space.space_spring.entity.User;
 
-@Builder
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class CreateCommentRequest {
-    private String content;
-    private boolean isReply;
-    private long targetId;
 
-    public Comment toEntity(User user, Post post) {
-        return Comment.builder()
-                .user(user)
-                .post(post)
-                .content(content)
-                .isReply(isReply)
-                .targetId(targetId)
-                .build();
+public class CreateCommentRequest {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Request {
+        private String content;
+        private boolean isReply;
+        @Nullable
+        private long targetId;
+
+        @Builder
+        public Comment toEntity(User user, Post post) {
+            return Comment.builder()
+                    .user(user)
+                    .post(post)
+                    .content(content)
+                    .isReply(isReply)
+                    .targetId(targetId)
+                    .build();
+        }
     }
 
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response {
+        private Long CommentId;
+
+        public static Response of(Long commentId) {
+            return new Response(commentId);
+        }
+    }
 }

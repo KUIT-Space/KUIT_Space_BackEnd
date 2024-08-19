b6fee216b589d54bfce4f2d3ddc5cd84283535eb
seongjunnoh <joon1754@naver.com>
2024-08-19 10:28:47 +0900

    fix : 정산 생성 request dto validation 어노테이션 수정

--- src/main/java/space/space_spring/dto/pay/request/PostPayCreateRequest.java
+++ src/main/java/space/space_spring/dto/pay/request/PostPayCreateRequest.java
@@ -1,6 +1,7 @@
 package space.space_spring.dto.pay.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +17,7 @@ public class PostPayCreateRequest {
     /**
      * PayRequest 엔티티 생성 시 필요한 정보
      */
-    @NotBlank(message = "총 정산 요청 금액은 공백일 수 없습니다.")
+    @NotNull(message = "총 정산 요청 금액은 공백일 수 없습니다.")
     private int totalAmount;
 
     @NotBlank(message = "은행 이름은 공백일 수 없습니다.")

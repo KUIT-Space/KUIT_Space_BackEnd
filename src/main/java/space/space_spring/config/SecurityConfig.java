87b990c04e907b7e59b83dc71b2d2fac23da7379
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2024-09-08 01:01:33 +0900

    [feat] SecurityConfig

--- src/main/java/space/space_spring/config/SecurityConfig.java
+++ src/main/java/space/space_spring/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +18,14 @@ public class SecurityConfig {
                 // csrf 설정을 disable
                 .csrf((csrfConfig) ->
                         csrfConfig.disable()
+                )
+                // HTTP 인증 요구 비활성화
+//                .authorizeHttpRequests(auth -> auth
+//                        .anyRequest().authenticated()
+//                )
+                // Session 사용 설정 해제
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 // h2 console 화면을 사용하기 위해 해당 옵션들 disable
                 .headers((headerConfig) ->

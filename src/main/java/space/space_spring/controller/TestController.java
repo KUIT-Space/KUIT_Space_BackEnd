a5eb5b9bc2aaf3a78dbbd7c7a9a5f01a07d2ec69
seongjunnoh <joon1754@naver.com>
2024-09-08 20:55:43 +0900

    feat : 카카오 로그인 시 refresh token 발급 추가

--- src/main/java/space/space_spring/controller/TestController.java
+++ src/main/java/space/space_spring/controller/TestController.java
@@ -53,25 +53,4 @@ public class TestController {
                 +"");
     }
 
-
-    /**
-     * 카카오 로그인 요청 처리
-     * 카카오 인증 서버의 인증 및 동의 요청 페이지로 redirect
-     */
-    @Value("${oauth.kakao.client.id}")
-    private String clientId;
-
-    @Value("${oauth.kakao.redirect.uri}")
-    private String redirectUri;
-
-    @GetMapping("/oauth/kakao")
-    public String kakaoConnect() {
-        StringBuffer url = new StringBuffer();
-        url.append("https://kauth.kakao.com/oauth/authorize?");
-        url.append("client_id="+clientId);
-        url.append("&redirect_uri="+redirectUri);
-        url.append("&response_type=code");
-        return "redirect:" + url.toString();
-    }
-
 }

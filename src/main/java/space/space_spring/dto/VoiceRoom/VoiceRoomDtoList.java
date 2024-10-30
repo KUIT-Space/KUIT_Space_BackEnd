3e2d837b4df23429fb377ff1e1f49b9d8c2aec64
seongjunnoh <joon1754@naver.com>
2024-10-30 21:19:11 +0900

    conflict 해결

--- src/main/java/space/space_spring/dto/VoiceRoom/VoiceRoomDtoList.java
+++ src/main/java/space/space_spring/dto/VoiceRoom/VoiceRoomDtoList.java
@@ -10,13 +10,17 @@ import space.space_spring.dao.UserSpaceDao;
 import space.space_spring.dao.VoiceRoomRepository;
 import space.space_spring.entity.Space;
 import space.space_spring.entity.User;
+import space.space_spring.entity.VoiceRoom;
 import space.space_spring.util.LiveKitUtils;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Service
 @RequiredArgsConstructor
 public class VoiceRoomDtoList {
@@ -26,16 +30,55 @@ public class VoiceRoomDtoList {
 //    private final UserSpaceDao userSpaceDao;
 //    private final UserDao userDao;
 //    private final TaskExecutor taskExecutor;
-    public void RoomDtoList(List<RoomDto> roomDtos){
+    public  VoiceRoomDtoList(List<RoomDto> roomDtos){
         this.roomDtoList=roomDtos;
     }
 
-    public void setActiveRoom(Map<Long,LivekitModels.Room> roomResponse){
+    public static VoiceRoomDtoList from(List<VoiceRoom> voiceRoomEntityList){
+        List<RoomDto> roomDtos = convertRoomDtoListByVoiceRoom(voiceRoomEntityList);
+        return new VoiceRoomDtoList(roomDtos);
+    }
+
+    public static List<RoomDto> convertRoomDtoListByVoiceRoom(List<VoiceRoom> voiceRoomList){
+        if(voiceRoomList==null||voiceRoomList.isEmpty()){return null;}
+        return voiceRoomList.stream()
+                .map(RoomDto::convertRoom)
+                .collect(Collectors.toList());
+    }
+
+    private void setActiveRoom(Map<String,LivekitModels.Room> roomResponse){
         for(RoomDto room : this.roomDtoList){
-            LivekitModels.Room resRoom= roomResponse.get(room.getId());
+            LivekitModels.Room resRoom= roomResponse.get(String.valueOf(room.getId()));
+            if(resRoom==null){continue;}
             room.setActiveRoom(resRoom);
         }
 
+    }
+    public void setActiveRoom(List<LivekitModels.Room> roomResponse){
+        Map<String,LivekitModels.Room> roomResMap=roomResponse.stream()
+                .collect(Collectors.toMap(
+                        res->res.getName(),
+                        res->res,
+                        (oldVal,newVal)->newVal
+                ));
+        roomResMap = Collections.unmodifiableMap(roomResMap);
+        setActiveRoom(roomResMap);
+    }
+
+    public List<GetVoiceRoomList.VoiceRoomInfo> convertVoicRoomInfoList(Integer limit){
+        if(this.roomDtoList==null||this.roomDtoList.isEmpty()){return null;}
+        Stream<RoomDto> sortedStream = this.roomDtoList.stream()
+                .sorted(Comparator
+                        .comparing((RoomDto r) -> r.getNumParticipants() == 0) // Active rooms first
+                        .thenComparing(RoomDto::getOrder)) ;// Then by order
+        System.out.print("limit input:"+limit);
+        Stream<RoomDto> processedStream = (limit != null) ? sortedStream.limit(limit) : sortedStream;
+
+        return processedStream.map(GetVoiceRoomList.VoiceRoomInfo::convertRoomDto)
+                .collect(Collectors.toList());
+    }
+    public List<GetVoiceRoomList.VoiceRoomInfo> convertVoicRoomInfoList() {
+        return convertVoicRoomInfoList( null);
     }
 
 //    public void setActiveRoom(List<LivekitModels.Room> roomResponsesTemp){

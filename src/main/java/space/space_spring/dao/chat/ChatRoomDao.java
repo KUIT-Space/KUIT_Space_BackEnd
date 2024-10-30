9268500aac3eb1a77fcc9b9bee9d70c0d26e0e80
hyunn522 <tjgus52213@gmail.com>
2024-10-30 21:09:33 +0900

    refactor: BaseStatus의 status 상수화

--- src/main/java/space/space_spring/dao/chat/ChatRoomDao.java
+++ src/main/java/space/space_spring/dao/chat/ChatRoomDao.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import space.space_spring.dao.chat.custom.ChatRoomDaoCustom;
 import space.space_spring.entity.ChatRoom;
+import space.space_spring.entity.enumStatus.BaseStatusType;
 
 @Repository
 public interface ChatRoomDao extends JpaRepository<ChatRoom, Long>, ChatRoomDaoCustom {
-    ChatRoom findByIdAndStatus(Long chatRoomId, String status);
+    ChatRoom findByIdAndStatus(Long chatRoomId, BaseStatusType status);
 }
--- src/main/java/space/space_spring/dao/chat/custom/ChatRoomDaoImpl.java
+++ src/main/java/space/space_spring/dao/chat/custom/ChatRoomDaoImpl.java
@@ -7,6 +7,7 @@ import space.space_spring.entity.Space;
 import space.space_spring.entity.User;
 
 import java.util.List;
+import space.space_spring.entity.enumStatus.BaseStatusType;
 
 import static space.space_spring.entity.QChatRoom.chatRoom;
 import static space.space_spring.entity.QUserChatRoom.userChatRoom;
@@ -23,9 +24,9 @@ public class ChatRoomDaoImpl implements ChatRoomDaoCustom {
                 .join(userChatRoom).on(
                         userChatRoom.chatRoom.eq(chatRoom)
                         .and(userChatRoom.user.eq(who))
-                        .and(userChatRoom.status.eq("ACTIVE")))
+                        .and(userChatRoom.status.eq(BaseStatusType.ACTIVE)))
                 .where(chatRoom.space.eq(where)
-                        .and(chatRoom.status.eq("ACTIVE")))
+                        .and(chatRoom.status.eq(BaseStatusType.ACTIVE)))
                 .orderBy(chatRoom.lastModifiedAt.desc())
                 .fetch();
     }

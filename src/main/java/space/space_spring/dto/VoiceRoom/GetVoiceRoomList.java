8d9dfd86e44dca3064c1262a1433aa6350cae2c5
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2024-11-04 01:45:52 +0900

    [refactor] add short function getParticipantDtoList

--- src/main/java/space/space_spring/dto/VoiceRoom/GetVoiceRoomList.java
+++ src/main/java/space/space_spring/dto/VoiceRoom/GetVoiceRoomList.java
@@ -48,7 +48,7 @@ public class GetVoiceRoomList {
                     .order(roomDto.getOrder())
                     .metadate(roomDto.getMetadata())
                     .participantInfoList(
-                            GetParticipantList.ParticipantInfo.convertParticipantDtoList(roomDto.getParticipantListDto().getParticipantDtoList())
+                            GetParticipantList.ParticipantInfo.convertParticipantDtoList(roomDto.getParticipantDtoList())
                     )
                     .build();
         }

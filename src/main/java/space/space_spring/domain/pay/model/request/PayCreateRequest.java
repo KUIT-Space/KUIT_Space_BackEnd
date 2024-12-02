bf4cabc65a0e329d9047e3dcc01b3d7d5a33b6d5
seongjunnoh <joon1754@naver.com>
2024-12-02 15:53:39 +0900

    test : PayController 테스트 코드 작성

--- src/main/java/space/space_spring/domain/pay/model/request/PayCreateRequest.java
+++ src/main/java/space/space_spring/domain/pay/model/request/PayCreateRequest.java
@@ -2,6 +2,9 @@ package space.space_spring.domain.pay.model.request;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import space.space_spring.domain.pay.model.PayCreateTargetInfo;
@@ -17,7 +20,9 @@ public class PayCreateRequest {
     /**
      * PayRequest 엔티티 생성 시 필요한 정보
      */
-    @NotBlank(message = "총 정산 요청 금액은 공백일 수 없습니다.")
+
+    @NotNull(message = "총 정산 요청 금액은 공백일 수 없습니다.")
+    @Positive(message = "총 정산 요청 금액은 양수이어야 합니다.")
     private int totalAmount;
 
     @NotBlank(message = "은행 이름은 공백일 수 없습니다.")
@@ -26,32 +31,20 @@ public class PayCreateRequest {
     @NotBlank(message = "은행 계좌 번호는 공백일 수 없습니다.")
     private String bankAccountNum;
 
-    /**
-     * PayRequestTarget 엔티티 생성 시 필요한 정보
-     * <targetUserId requestedAmount> 쌍
-     */
     @Valid
-    private List<PayCreateTargetRequest> targetInfoRequests;
+    private List<TargetInfoRequest> targetInfoRequests;
 
     @EnumValidator(enumClass = PayType.class, message = "payType은 INDIVIDUAL 또는 EQUAL_SPLIT 이어야 합니다.")
     private String payType;
 
-    @Getter
-    @NoArgsConstructor
-    private static class PayCreateTargetRequest {
 
-        @NotBlank(message = "정산 요청 타겟 유저의 id값은 공백일 수 없습니다.")
-        private Long targetUserId;
-
-        @NotBlank(message = "정산 요청 금액은 공백일 수 없습니다.")
-        private int requestedAmount;
-
-        public PayCreateTargetInfo toPayCreateTargetInfo() {
-            return PayCreateTargetInfo.builder()
-                    .targetUserId(targetUserId)
-                    .requestedAmount(requestedAmount)
-                    .build();
-        }
+    @Builder
+    private PayCreateRequest(int totalAmount, String bankName, String bankAccountNum, List<TargetInfoRequest> targetInfoRequests, String payType) {
+        this.totalAmount = totalAmount;
+        this.bankName = bankName;
+        this.bankAccountNum = bankAccountNum;
+        this.targetInfoRequests = targetInfoRequests;
+        this.payType = payType;
     }
 
     public PayCreateServiceRequest toServiceRequest() {
@@ -60,7 +53,7 @@ public class PayCreateRequest {
                 .bankName(bankName)
                 .bankAccountNum(bankAccountNum)
                 .payCreateTargetInfos(targetInfoRequests.stream()
-                        .map(PayCreateTargetRequest::toPayCreateTargetInfo)
+                        .map(TargetInfoRequest::toPayCreateTargetInfo)
                         .toList())
                 .payType(PayType.valueOf(payType))
                 .build();

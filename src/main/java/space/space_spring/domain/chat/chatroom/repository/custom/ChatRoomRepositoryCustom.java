34ba4bfa0ed581e33c3f0d30d653cdcb38f97a78
hyunn522 <tjgus52213@gmail.com>
2025-01-16 15:38:38 +0900

    chore: domain 디렉토리 정리

--- src/main/java/space/space_spring/domain/chat/chatroom/repository/custom/ChatRoomRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package space.space_spring.domain.chat.chatroom.repository.custom;
-
-import space.space_spring.domain.chat.chatroom.model.ChatRoom;
-
-import java.util.List;
-
-public interface ChatRoomRepositoryCustom {
-    List<ChatRoom> findByUserIdAndSpaceId(Long userId, Long spaceId);
-}
--- src/main/java/space/space_spring/domain/pay/model/PayAmountPolicy.java
+++
@@ -1,8 +0,0 @@
-package space.space_spring.domain.pay.model;
-
-import java.util.List;
-
-public interface PayAmountPolicy {
-
-    void validatePayAmount(int totalAmount, List<Integer> targetAmounts);
-}

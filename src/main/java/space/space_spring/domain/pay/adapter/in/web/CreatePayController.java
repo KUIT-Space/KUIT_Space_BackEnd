aaad8fd02b4a9f8d2157958874022ea47b126fd9
seongjunnoh <joon1754@naver.com>
2025-01-24 18:55:54 +0900

    feat : 정산 생성 api request 유효성 검증 로직 추가

--- src/main/java/space/space_spring/domain/pay/adapter/in/web/CreatePayController.java
+++ src/main/java/space/space_spring/domain/pay/adapter/in/web/CreatePayController.java
@@ -1,12 +1,18 @@
 package space.space_spring.domain.pay.adapter.in.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import space.space_spring.domain.pay.application.port.in.CreatePayCommand;
 import space.space_spring.domain.pay.application.port.in.CreatePayUseCase;
 import space.space_spring.global.argumentResolver.jwtLogin.JwtLoginAuth;
+import space.space_spring.global.exception.CustomException;
+
+import static space.space_spring.global.common.response.status.BaseExceptionResponseStatus.INVALID_PAY_CREATE;
+import static space.space_spring.global.util.bindingResult.BindingResultUtils.getErrorMessage;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +21,10 @@ public class CreatePayController {
     private final CreatePayUseCase createPayUseCase;
 
     @PostMapping("/pay/create")
-    public String createPay(@JwtLoginAuth Long id, @RequestBody RequestOfCreatePay request) {
+    public String createPay(@JwtLoginAuth Long id, @Validated @RequestBody RequestOfCreatePay request, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new CustomException(INVALID_PAY_CREATE, getErrorMessage(bindingResult));
+        }
 
         CreatePayCommand createPayCommand = CreatePayCommand.create(id, request.getTotalAmount(), request.getBankName(), request.getBankAccountNum(), request.getTargets(), request.getValueOfPayType());
         createPayUseCase.createPay(createPayCommand);
--- src/main/java/space/space_spring/domain/pay/adapter/in/web/TargetOfPayRequest.java
+++ src/main/java/space/space_spring/domain/pay/adapter/in/web/TargetOfPayRequest.java
@@ -1,17 +1,30 @@
 package space.space_spring.domain.pay.adapter.in.web;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import space.space_spring.global.common.validation.SelfValidating;
 
 @Getter
 @NoArgsConstructor
-public class TargetOfPayRequest {
+public class TargetOfPayRequest extends SelfValidating<TargetOfPayRequest> {
 
     /**
-     * 입력 유효성 검사 수행 -> not null, 정산 요청액은 양수의 정수여야 함
+     * 입력 유효성 검사 수행
      */
 
+    @NotNull(message = "정산 요청 타겟 유저의 id값은 공백일 수 없습니다.")
     private Long targetMemberId;
 
+    @NotNull(message = "정산 요청 금액은 공백일 수 없습니다.")
+    @Positive(message = "정산 요청 금액은 양수이어야 합니다.")
     private int requestedAmount;
+
+    public TargetOfPayRequest(Long targetMemberId, int requestedAmount) {
+        this.targetMemberId = targetMemberId;
+        this.requestedAmount = requestedAmount;
+        this.validateSelf();
+    }
+
 }

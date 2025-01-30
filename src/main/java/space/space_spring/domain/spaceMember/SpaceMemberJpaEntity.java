fad200c6b7592d41a1f9502c4e4dc62c0952d565
seongjunnoh <joon1754@naver.com>
2025-01-30 16:32:06 +0900

    refactor : jpa 엔티티 테이블, 컬럼 네이밍 erd대로 수정

--- src/main/java/space/space_spring/domain/spaceMember/SpaceMemberJpaEntity.java
+++ src/main/java/space/space_spring/domain/spaceMember/SpaceMemberJpaEntity.java
@@ -1,9 +1,6 @@
 package space.space_spring.domain.spaceMember;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Getter;
 import space.space_spring.domain.space.SpaceJpaEntity;
 import space.space_spring.domain.user.UserJpaEntity;
@@ -11,17 +8,22 @@ import space.space_spring.global.common.entity.BaseEntity;
 
 @Entity
 @Getter
+@Table(name = "Space_Member")
 public class SpaceMemberJpaEntity extends BaseEntity {
 
     @Id @GeneratedValue
+    @Column(name = "space_member_id")
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "space_id")
     private SpaceJpaEntity space;
 
     @ManyToOne
+    @JoinColumn(name = "user_id")
     private UserJpaEntity user;
 
+    @Column(name = "discord_member_id")
     private Long discordId;         // 디스코드 id 값
 
 

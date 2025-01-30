3d2819de2ecab45f1508541ce5725c461335dd28
seongjunnoh <joon1754@naver.com>
2025-01-30 16:09:55 +0900

    feat : ReadPayRequestList 영속성 계층 코드 작성

--- src/main/java/space/space_spring/domain/pay/adapter/out/persistence/PayRequestMapper.java
+++ src/main/java/space/space_spring/domain/pay/adapter/out/persistence/PayRequestMapper.java
@@ -1,8 +1,13 @@
 package space.space_spring.domain.pay.adapter.out.persistence;
 
 import org.springframework.stereotype.Component;
+import space.space_spring.domain.pay.domain.Bank;
+import space.space_spring.domain.pay.domain.Money;
 import space.space_spring.domain.pay.domain.PayRequest;
+import space.space_spring.domain.pay.domain.PayType;
+import space.space_spring.domain.spaceMember.SpaceMember;
 import space.space_spring.domain.spaceMember.SpaceMemberJpaEntity;
+import space.space_spring.global.util.NaturalNumber;
 
 @Component
 public class PayRequestMapper {
@@ -11,9 +16,21 @@ public class PayRequestMapper {
         return PayRequestJpaEntity.create(
                 payCreatorJpaEntity,
                 domain.getTotalAmount().getAmountInInteger(),
+                domain.getTotalTargetNum().getNumber(),
                 domain.getBank().getName(),
                 domain.getBank().getAccountNumber(),
                 domain.getPayType()
         );
+    }
+
+    public PayRequest toDomainEntity(SpaceMember payCreator, PayRequestJpaEntity jpaEntity) {
+        Bank bank = Bank.of(jpaEntity.getBankName(), jpaEntity.getBankAccountNum());
+
+        return PayRequest.create(
+                payCreator,
+                Money.of(jpaEntity.getTotalAmount()),
+                NaturalNumber.of(jpaEntity.getTotalTargetNum()),
+                bank,
+                jpaEntity.getPayType());
     }
 }
--- src/main/java/space/space_spring/domain/pay/adapter/out/persistence/SpringDataPayRequestRepository.java
+++ src/main/java/space/space_spring/domain/pay/adapter/out/persistence/SpringDataPayRequestRepository.java
@@ -1,7 +1,12 @@
 package space.space_spring.domain.pay.adapter.out.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import space.space_spring.domain.spaceMember.SpaceMemberJpaEntity;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface SpringDataPayRequestRepository extends JpaRepository<PayRequestJpaEntity, Long> {
 
+    Optional<List<PayRequestJpaEntity>> findByPayCreator(SpaceMemberJpaEntity payCreator);
 }

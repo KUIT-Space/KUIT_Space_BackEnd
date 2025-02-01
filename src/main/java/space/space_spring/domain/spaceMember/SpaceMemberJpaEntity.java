eebc0a41f8ca7e64bec42026c76bf6388ec48e09
seongjunnoh <joon1754@naver.com>
2025-02-01 16:04:28 +0900

    fix : SpaceMemberJpaEntity 수정

--- src/main/java/space/space_spring/domain/spaceMember/SpaceMemberJpaEntity.java
+++ src/main/java/space/space_spring/domain/spaceMember/SpaceMemberJpaEntity.java
@@ -1,28 +1,41 @@
 package space.space_spring.domain.spaceMember;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import space.space_spring.domain.space.SpaceJpaEntity;
 import space.space_spring.domain.user.UserJpaEntity;
 import space.space_spring.global.common.entity.BaseEntity;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "Space_Member")
 public class SpaceMemberJpaEntity extends BaseEntity {
 
     @Id @GeneratedValue
+    @Column(name = "space_member_id")
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "space_id")
     private SpaceJpaEntity space;
 
     @ManyToOne
+    @JoinColumn(name = "user_id")
     private UserJpaEntity user;
 
+    @Column(name = "discord_member_id")
     private Long discordId;         // 디스코드 id 값
 
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+
+    @Column(name = "is_manager")
+    private boolean isManager;
 
 }

c4cfa832606d598ddae897ba42203593ace812dc
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2025-02-05 01:29:05 +0900

    [feat] add "init-space" & "set-board" command
    add two command and register method activate

--- src/main/java/space/space_spring/global/config/JDA/SlashCommandRegistrar.java
+++ src/main/java/space/space_spring/global/config/JDA/SlashCommandRegistrar.java
@@ -3,18 +3,26 @@ package space.space_spring.global.config.JDA;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import static net.dv8tion.jda.api.interactions.commands.OptionType.STRING;
 
 @Component
-@RequiredArgsConstructor
+
 public class SlashCommandRegistrar {
     private final JDA jda;
 
-    private void registerCommand(){
+    @Autowired
+    public SlashCommandRegistrar(JDA jda) {
+        this.jda = jda;
+        this.registerCommands();
+    }
+    private void registerCommands(){
         CommandListUpdateAction commands = jda.updateCommands();
 
         commands.addCommands(
@@ -33,6 +41,15 @@ public class SlashCommandRegistrar {
         commands.addCommands(
                 Commands.slash("channel-info","get this Channel info")
         );
+
+        commands.addCommands(
+                Commands.slash("set-board","get this Channel info")
+                        .addOptions(
+                                new OptionData(OptionType.STRING, "board-type", "게시판 종류를 선택해주세요")
+                                .addChoice("Board", "일반 게시판입니다. 익명 기능이 없습니다")
+                                .addChoice("question-Board", "질문 게시판입니다. 익명 기능이 있습니다.")
+
+        ));
 
         commands.queue();
     }

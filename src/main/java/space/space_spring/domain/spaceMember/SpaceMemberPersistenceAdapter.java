8e666e12dfe6f8039c2795149339463034680746
hyunn522 <tjgus52213@gmail.com>
2025-02-05 21:37:11 +0900

    chore: 클래스 위치 변경

--- src/main/java/space/space_spring/domain/spaceMember/SpaceMemberPersistenceAdapter.java
+++ src/main/java/space/space_spring/domain/spaceMember/SpaceMemberPersistenceAdapter.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Repository;
 import space.space_spring.domain.pay.application.port.out.LoadSpaceMemberPort;
 import space.space_spring.domain.space.Space;
 import space.space_spring.domain.space.SpaceMapper;
-import space.space_spring.domain.user.User;
-import space.space_spring.domain.user.UserMapper;
+import space.space_spring.domain.user.domain.User;
+import space.space_spring.domain.user.adapter.out.persistence.UserMapper;
 import space.space_spring.global.exception.CustomException;
 
 import static space.space_spring.global.common.response.status.BaseExceptionResponseStatus.SPACE_MEMBER_NOT_FOUND;

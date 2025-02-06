a442b7f991871ce36814bf092cc304bb1a4057d0
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2025-02-06 17:27:03 +0900

    [refactor] change createSpace return and annotation

--- src/main/java/space/space_spring/domain/space/application/service/CreateSpaceService.java
+++ src/main/java/space/space_spring/domain/space/application/service/CreateSpaceService.java
@@ -25,10 +25,23 @@ public class CreateSpaceService implements CreateSpaceUseCase {
     @Override
     @Transactional
     public Long createSpace(CreateSpaceCommand command){
-
+        //space가 기존에 등록이 되었는지 확인
+        //이미 등록된 guild라면 예외 발생
         checkSpacePresent(loadSpacePort.loadSpaceByDiscordId(command.getGuildId()));
-        return createSpacePort.saveSpace(command.getGuildId(),
+
+        //space 생성
+        Space newSpace = createSpacePort.saveSpace(command.getGuildId(),
                 command.getGuildName());
+
+        //GuildMember 정보 가져오기
+
+        //User가 없는 경우 User 생성
+
+        //SpaceMember 생성
+
+
+
+        return newSpace.getId();
     }
 
     private void checkSpacePresent(Optional<Space> space){

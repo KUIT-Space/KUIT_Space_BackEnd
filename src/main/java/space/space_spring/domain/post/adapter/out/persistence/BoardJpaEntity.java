f65386de27a636e643f723cd981e40c71fea340f
arkchive <kyungminkim7@konkuk.ac.kr>
2025-02-08 01:04:07 +0900

    feat: 게시판 생성 api 추가

--- src/main/java/space/space_spring/domain/post/adapter/out/persistence/BoardJpaEntity.java
+++ src/main/java/space/space_spring/domain/post/adapter/out/persistence/BoardJpaEntity.java
@@ -3,7 +3,6 @@ package space.space_spring.domain.post.adapter.out.persistence;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import space.space_spring.domain.post.domain.BoardType;
@@ -31,25 +30,28 @@ public class BoardJpaEntity extends BaseEntity {
     @NotNull
     private Long discordId;
 
-    @Column(name = "board_name")
-    @NotNull
-    private String name;
-
     @Enumerated(EnumType.STRING)
     @Column(name = "board_type")
     @NotNull
     private BoardType boardType;
 
+    @Column(name = "board_name")
+    @NotNull
+    private String boardName;
+
     @Column(name = "webhook_url")
     @NotNull
     private String webhookUrl;
 
-    @Builder
-    private BoardJpaEntity(SpaceJpaEntity space, Long discordId, String name, BoardType boardType, String webhookUrl) {
+    private BoardJpaEntity(SpaceJpaEntity space, Long discordId, BoardType boardType, String boardName, String webhookUrl) {
         this.space = space;
         this.discordId = discordId;
-        this.name = name;
+        this.boardName = boardName;
         this.boardType = boardType;
         this.webhookUrl = webhookUrl;
+    }
+
+    public static BoardJpaEntity create(SpaceJpaEntity space, Long discordId, BoardType boardType, String boardName, String webhookUrl) {
+        return new BoardJpaEntity(space, discordId, boardType, boardName, webhookUrl);
     }
 }
--- src/main/java/space/space_spring/domain/post/domain/BoardType.java
+++ src/main/java/space/space_spring/domain/post/domain/BoardType.java
@@ -6,5 +6,13 @@ import lombok.Getter;
 public enum BoardType {
     POST,
     QUESTION,
-    COMMENT
+    COMMENT;
+
+    public static BoardType fromString(String stringOfBoardType) {
+        try {
+            return BoardType.valueOf(stringOfBoardType.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("존재하지 않는 BoardType입니다. 사용 가능한 값: [POST, QUESTION, COMMENT], 입력값: " + stringOfBoardType);
+        }
+    }
 }

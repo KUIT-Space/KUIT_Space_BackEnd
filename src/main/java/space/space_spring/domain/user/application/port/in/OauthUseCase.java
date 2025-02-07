14122616513f5ce16aa4b927690118600bcb8eac
hyunn522 <tjgus52213@gmail.com>
2025-02-07 16:10:07 +0900

    feat: 디스코드와의 oauth 통신을 port로 추출

--- src/main/java/space/space_spring/domain/user/application/port/in/OauthUseCase.java
+++ src/main/java/space/space_spring/domain/user/application/port/in/OauthUseCase.java
@@ -1,13 +1,10 @@
 package space.space_spring.domain.user.application.port.in;
 
+
 import com.fasterxml.jackson.core.JsonProcessingException;
-import space.space_spring.domain.user.domain.User;
 
 public interface OauthUseCase {
 
-    String getAccessToken(String code) throws JsonProcessingException;
+    String signInWithDiscord(String code) throws JsonProcessingException;
 
-    User getUserInfo(String accessToken) throws JsonProcessingException;
-
-    String signIn(User user);
 }

79b8e9ee15aa6cefaa5584765c0dbdc4e83b3a82
arkchive <kyungminkim7@konkuk.ac.kr>
2025-02-07 15:25:08 +0900

    refactor: board 도메인 엔티티 수정

--- src/main/java/space/space_spring/domain/post/domain/Board.java
+++ src/main/java/space/space_spring/domain/post/domain/Board.java
@@ -1,34 +1,33 @@
 package space.space_spring.domain.post.domain;
 
 import lombok.Getter;
-import space.space_spring.domain.space.domain.Space;
 
 @Getter
 public class Board {
 
     private Long id;
 
-    private Space space;
+    private Long spaceId;
 
     private Long discordId;
 
-    private String name;
+    private String boardName;
 
     private BoardType boardType;
 
     private String webhookUrl;
 
-    private Board(Long id, Space space, Long discordId, String name, BoardType boardType, String webhookUrl) {
+    private Board(Long id, Long spaceId, Long discordId, String boardName, BoardType boardType, String webhookUrl) {
         this.id = id;
-        this.space = space;
+        this.spaceId = spaceId;
         this.discordId = discordId;
-        this.name = name;
+        this.boardName = boardName;
         this.boardType = boardType;
         this.webhookUrl = webhookUrl;
     }
 
     // 정적 팩토리 메서드
-    public static Board of(Long id, Space space, Long discordId, String name, BoardType boardType, String webhookUrl) {
-        return new Board(id, space, discordId, name, boardType, webhookUrl);
+    public static Board of(Long id, Long spaceId, Long discordId, String boardName, BoardType boardType, String webhookUrl) {
+        return new Board(id, spaceId, discordId, boardName, boardType, webhookUrl);
     }
 }

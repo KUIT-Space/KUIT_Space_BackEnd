e1f9e44a1e62eb21a96dcd9c5e8164bd50dae5f4
hyunn522 <tjgus52213@gmail.com>
2025-02-07 16:25:56 +0900

    feat: 로그인 시 RefreshToken 발급

--- src/main/java/space/space_spring/domain/user/adapter/in/web/OauthController.java
+++ src/main/java/space/space_spring/domain/user/adapter/in/web/OauthController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import space.space_spring.domain.user.application.port.in.OauthUseCase;
-import space.space_spring.domain.user.domain.User;
 import space.space_spring.global.common.response.BaseResponse;
 import space.space_spring.global.common.response.SuccessResponse;
 
@@ -19,12 +18,14 @@ public class OauthController {
 
     @GetMapping("/oauth/discord")
     public BaseResponse<SuccessResponse> signInDiscord(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
-        String token = oauthUseCase.signInWithDiscord(code);
-        response.setHeader("Authorization", "Bearer " + token);
+        TokenPair tokenPair = oauthUseCase.signInWithDiscord(code);
 
-        if (token == null || token.isEmpty()) {
+        if (tokenPair == null) {
             return new BaseResponse<>(new SuccessResponse(false));
         }
+
+        response.setHeader("Authorization", "Bearer " + tokenPair.getAccessToken());
+        response.setHeader("Authorization-refresh", "Bearer " + tokenPair.getRefreshToken());
         return new BaseResponse<>(new SuccessResponse(true));
     }
 

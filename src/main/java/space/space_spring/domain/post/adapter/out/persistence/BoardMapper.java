1c37be9d599e883d4e7900ba97050732bb1919ce
arkchive <kyungminkim7@konkuk.ac.kr>
2025-02-11 14:37:16 +0900

    refactor: toDomain 파라미터 수정

--- src/main/java/space/space_spring/domain/post/adapter/out/persistence/BoardMapper.java
+++ src/main/java/space/space_spring/domain/post/adapter/out/persistence/BoardMapper.java
@@ -3,7 +3,6 @@ package space.space_spring.domain.post.adapter.out.persistence;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import space.space_spring.domain.post.domain.Board;
-import space.space_spring.domain.post.domain.BoardType;
 import space.space_spring.domain.space.SpaceMapper;
 import space.space_spring.domain.space.domain.SpaceJpaEntity;
 
@@ -25,10 +24,10 @@ public class BoardMapper {
     }
 
     // JPA 엔티티 -> 도메인
-    public Board toDomain(Long spaceId, BoardJpaEntity jpaEntity) {
+    public Board toDomain(BoardJpaEntity jpaEntity) {
         return Board.of(
                 jpaEntity.getId(),
-                spaceId,
+                jpaEntity.getSpace().getId(),
                 jpaEntity.getDiscordId(),
                 jpaEntity.getBoardName(),
                 jpaEntity.getBoardType(),

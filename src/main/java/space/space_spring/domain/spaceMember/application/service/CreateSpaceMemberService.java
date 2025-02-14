801959834e04ff53adcacd3936c5c58631d1bd0e
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2025-02-15 03:26:07 +0900

    [feat]spaceMember create List

--- src/main/java/space/space_spring/domain/spaceMember/application/service/CreateSpaceMemberService.java
+++ src/main/java/space/space_spring/domain/spaceMember/application/service/CreateSpaceMemberService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import space.space_spring.domain.spaceMember.application.port.in.CreateSpaceMemberUseCase;
 import space.space_spring.domain.spaceMember.application.port.out.CreateSpaceMemberPort;
 import space.space_spring.domain.spaceMember.domian.SpaceMember;
+import space.space_spring.domain.spaceMember.domian.SpaceMembers;
+
 @Service
 @RequiredArgsConstructor
 public class CreateSpaceMemberService implements CreateSpaceMemberUseCase {
@@ -13,5 +15,11 @@ public class CreateSpaceMemberService implements CreateSpaceMemberUseCase {
     @Override
     public SpaceMember create(SpaceMember spaceMember){
         return createSpaceMemberPort.createSpaceMember(spaceMember);
+    }
+
+    @Override
+    public SpaceMembers create(SpaceMembers spaceMembers){
+
+        return SpaceMembers.of(createSpaceMemberPort.createSpaceMembers(spaceMembers.toStream().toList()));
     }
 }

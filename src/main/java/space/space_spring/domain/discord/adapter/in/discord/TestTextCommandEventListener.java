3363cada2a2f9d7e89141d38e75f8627c95fb6ae
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2025-02-18 01:45:14 +0900

    [test] add mention test text command in discord

--- src/main/java/space/space_spring/domain/discord/adapter/in/discord/TestTextCommandEventListener.java
+++ src/main/java/space/space_spring/domain/discord/adapter/in/discord/TestTextCommandEventListener.java
@@ -3,6 +3,7 @@ package space.space_spring.domain.discord.adapter.in.discord;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Mentions;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -88,6 +89,12 @@ public class TestTextCommandEventListener extends ListenerAdapter {
                 msg.getChannel().sendMessage("\n"+spaceMember.getNickname()+":"+spaceMember.getId()).queue();
             });
             return;
+        }
+
+        if(msg.getContentRaw().equals("!mention")) {
+
+
+            event.getChannel().sendMessage("hi"+event.getMember().getAsMention()).queue();
         }
     }
 

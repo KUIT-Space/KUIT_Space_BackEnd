9213b11236f304310e8564c5fdc6d0245dac7f8e
hyunn522 <tjgus52213@gmail.com>
2025-02-17 19:59:50 +0900

    feat: 토큰 생성 시 해당 유저의 spaceMemberId 사용

--- src/main/java/space/space_spring/domain/spaceMember/adapter/out/persistence/SpringDataSpaceMemberRepository.java
+++ src/main/java/space/space_spring/domain/spaceMember/adapter/out/persistence/SpringDataSpaceMemberRepository.java
@@ -1,5 +1,6 @@
 package space.space_spring.domain.spaceMember.adapter.out.persistence;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import space.space_spring.domain.spaceMember.domian.SpaceMemberJpaEntity;
 
@@ -7,5 +8,5 @@ import java.util.List;
 
 public interface SpringDataSpaceMemberRepository extends JpaRepository<SpaceMemberJpaEntity, Long> {
     List<SpaceMemberJpaEntity> findBySpaceId(Long spaceId);
-
+    Optional<SpaceMemberJpaEntity> findByUserId(Long userId);
 }

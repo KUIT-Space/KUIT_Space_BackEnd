6292564809fccaf3e738713332c460ea9f389098
hyunn522 <tjgus52213@gmail.com>
2025-02-17 19:27:23 +0900

    feat: jwt 생성에 사용되는 id 변경

--- src/main/java/space/space_spring/domain/authorization/jwt/model/JwtLoginProvider.java
+++ src/main/java/space/space_spring/domain/authorization/jwt/model/JwtLoginProvider.java
@@ -32,22 +32,22 @@ public class JwtLoginProvider {
     @Value("${secret.jwt.refresh-expired-in}")
     private Long REFRESH_EXPIRED_IN;
 
-    public String generateToken(Long userId, TokenType tokenType) {
+    public String generateToken(Long spaceMemberId, TokenType tokenType) {
 //        Claims claims = Jwts.claims().setSubject(jwtPayloadDto.getUserId().toString());
 
         Date now = new Date();
         Date expiration = setExpiration(now, tokenType);
 
-        return makeToken(tokenType, userId, now, expiration);
+        return makeToken(tokenType, spaceMemberId, now, expiration);
     }
 
-    private String makeToken(TokenType tokenType, Long userId, Date now, Date expiration) {
+    private String makeToken(TokenType tokenType, Long spaceMemberId, Date now, Date expiration) {
         if (tokenType.equals(TokenType.ACCESS)) {
             return Jwts.builder()
 //                .setClaims(claims)
                     .setIssuedAt(now)
                     .setExpiration(expiration)
-                    .claim("userId", userId)
+                    .claim("spaceMemberId", spaceMemberId)
                     .signWith(SignatureAlgorithm.HS256, choiceSecretKey(tokenType))
                     .compact();
         }
@@ -102,15 +102,15 @@ public class JwtLoginProvider {
 
     }
 
-    public Long getUserIdFromAccessToken(String token) {
+    public Long getSpaceMemberIdFromAccessToken(String token) {
         try {
             Jws<Claims> claims = Jwts.parserBuilder()
                     .setSigningKey(ACCESS_SECRET_KEY).build()
                     .parseClaimsJws(token);
-            return claims.getBody().get("userId", Long.class);
+            return claims.getBody().get("spaceMemberId", Long.class);
         } catch (ExpiredJwtException e) {
             // 만료된 토큰에서 userId 추출
-            return e.getClaims().get("userId", Long.class);
+            return e.getClaims().get("spaceMemberId", Long.class);
         } catch (JwtException e) {
             log.error("[JwtTokenProvider.getJwtPayloadDtoFromToken]", e);
             throw e;

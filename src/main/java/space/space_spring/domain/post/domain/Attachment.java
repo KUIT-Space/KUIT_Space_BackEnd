919a8631f4c92d2d4b8db7dfebe204a49dbe44d9
arkchive <kyungminkim7@konkuk.ac.kr>
2025-02-18 15:35:43 +0900

    refactor: PostBase 도메인 엔티티 삭제로 인한 도메인 엔티티 매핑 관계 수정

--- src/main/java/space/space_spring/domain/post/domain/Attachment.java
+++ src/main/java/space/space_spring/domain/post/domain/Attachment.java
@@ -7,24 +7,35 @@ public class Attachment{
 
     private Long id;
 
-    private Long postBaseId;
+    private Long postId;
+
+    private Long questionId;
 
     private AttachmentType attachmentType;
 
     private String attachmentUrl;
 
-    private Attachment(Long id, Long postBaseId, AttachmentType attachmentType, String attachmentUrl) {
+    private Attachment(Long id, Long postId, Long questionId, AttachmentType attachmentType, String attachmentUrl) {
         this.id = id;
-        this.postBaseId = postBaseId;
+        this.postId = postId;
+        this.questionId = questionId;
         this.attachmentType = attachmentType;
         this.attachmentUrl = attachmentUrl;
     }
 
-    public static Attachment of(Long id, Long postBaseId, AttachmentType attachmentType, String attachmentUrl) {
-        return new Attachment(id, postBaseId, attachmentType, attachmentUrl);
+    public static Attachment createForPost(Long id, Long postId, AttachmentType attachmentType, String attachmentUrl) {
+        return new Attachment(id, postId, null, attachmentType, attachmentUrl);
     }
 
-    public static Attachment withoutId(Long postBaseId, AttachmentType attachmentType, String attachmentUrl) {
-        return new Attachment(null, postBaseId, attachmentType, attachmentUrl);
+    public static Attachment withoutIdForPost(Long postId, AttachmentType attachmentType, String attachmentUrl) {
+        return new Attachment(null, postId, null, attachmentType, attachmentUrl);
+    }
+
+    public static Attachment createForQuestion(Long id, Long questionId, AttachmentType attachmentType, String attachmentUrl) {
+        return new Attachment(id, null, questionId, attachmentType, attachmentUrl);
+    }
+
+    public static Attachment withoutIdForQuestion(Long questionId, AttachmentType attachmentType, String attachmentUrl) {
+        return new Attachment(null, null, questionId, attachmentType, attachmentUrl);
     }
 }

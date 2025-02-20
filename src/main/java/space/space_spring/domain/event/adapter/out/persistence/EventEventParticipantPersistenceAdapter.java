532bc9c39ac7f348bb9cc402667b278ed950ada1
hyunn522 <tjgus52213@gmail.com>
2025-02-21 00:24:55 +0900

    feat: event 참여 영속성 계층 추가

--- src/main/java/space/space_spring/domain/event/adapter/out/persistence/EventEventParticipantPersistenceAdapter.java
+++ src/main/java/space/space_spring/domain/event/adapter/out/persistence/EventEventParticipantPersistenceAdapter.java
@@ -6,17 +6,25 @@ import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import space.space_spring.domain.event.application.port.out.CreateEventParticipantPort;
 import space.space_spring.domain.event.application.port.out.LoadEventParticipantPort;
+import space.space_spring.domain.event.domain.Event;
 import space.space_spring.domain.event.domain.EventParticipant;
 import space.space_spring.domain.event.domain.EventParticipants;
+import space.space_spring.domain.spaceMember.adapter.out.persistence.SpaceMemberMapper;
+import space.space_spring.domain.spaceMember.adapter.out.persistence.SpringDataSpaceMemberRepository;
+import space.space_spring.domain.spaceMember.domian.SpaceMember;
+import space.space_spring.domain.spaceMember.domian.SpaceMemberJpaEntity;
 import space.space_spring.global.exception.CustomException;
 
 @RequiredArgsConstructor
 @Repository
-public class EventParticipantPersistenceAdapter implements LoadEventParticipantPort {
+public class EventEventParticipantPersistenceAdapter implements LoadEventParticipantPort, CreateEventParticipantPort {
 
     private final EventRepository eventRepository;
     private final EventParticipantRepository eventParticipantRepository;
+    private final SpringDataSpaceMemberRepository spaceMemberRepository;
+    private final EventMapper eventMapper;
     private final EventParticipantMapper eventParticipantMapper;
 
     @Override
@@ -32,4 +40,16 @@ public class EventParticipantPersistenceAdapter implements LoadEventParticipantP
 
         return EventParticipants.create(participants);
     }
+
+    @Override
+    public EventParticipant createParticipant(EventParticipant participant) {
+        EventJpaEntity event = eventRepository.findById(participant.getEventId())
+                .orElseThrow(() -> new CustomException(EVENT_NOT_FOUND));
+        SpaceMemberJpaEntity spaceMember = spaceMemberRepository.findById(participant.getSpaceMemberId())
+                .orElseThrow(() -> new CustomException(SPACE_MEMBER_NOT_FOUND));
+
+        EventParticipantJpaEntity savedParticipant = eventParticipantRepository.save(eventParticipantMapper.toJpaEntity(event, spaceMember));
+        return eventParticipantMapper.toDomainEntity(savedParticipant);
+    }
+
 }

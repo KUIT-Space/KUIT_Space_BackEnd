c3b381b793192d8c256a684e173a98dab5bb2edd
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2025-02-24 16:25:48 +0900

    [feat] check role exist code

--- src/main/java/space/space_spring/domain/discord/adapter/out/DiscordRoleAdapter.java
+++ src/main/java/space/space_spring/domain/discord/adapter/out/DiscordRoleAdapter.java
@@ -7,21 +7,42 @@ import net.dv8tion.jda.api.entities.Role;
 import org.springframework.stereotype.Component;
 import space.space_spring.domain.discord.application.port.out.CreateDiscordRolePort;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class DiscordRoleAdapter implements CreateDiscordRolePort {
     private final JDA jda;
     @Override
     public boolean createRole(Long guildId,String name,Integer rgb){
-        jda.getGuildById(guildId).createRole().setName(name).setColor(rgb).complete();
-        return true;
+        if(getRoleExist(guildId,name).isEmpty()){
+            createRoleInDiscord(guildId, name, rgb);
+            return true;
+        }else{
+            return false;
+        }
     }
     @Override
     public boolean createAndAddRole(Long guildId,String name,Integer rgb,Long memberId){
         Guild guild=jda.getGuildById(guildId);
-        Role role = guild.createRole().setName(name).setColor(rgb).complete();
+
+        List<Role> roles = getRoleExist(guildId, name);
+        Role role;
+        if(roles.isEmpty()){
+            role=createRoleInDiscord(guildId, name, rgb);
+        }else{
+            role=roles.get(0);
+        }
         guild.addRoleToMember(jda.getUserById(memberId),role).complete();
 
         return true;
+    }
+    private Role createRoleInDiscord(Long guildId,String name,Integer rgb){
+        return jda.getGuildById(guildId).createRole().setName(name).setColor(rgb).complete();
+    }
+
+    private List<Role> getRoleExist(Long guildId, String name){
+        return jda.getGuildById(guildId).getRolesByName(name,true);
+
     }
 }

1ab7e1cb80ac33806bee40201136c4055f2ccc31
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2025-03-03 14:51:33 +0900

    [feat] create channel testText command
    테스트용 게시물 생성 기능

--- src/main/java/space/space_spring/domain/discord/adapter/in/discord/TestTextCommandEventListener.java
+++ src/main/java/space/space_spring/domain/discord/adapter/in/discord/TestTextCommandEventListener.java
@@ -4,10 +4,15 @@ import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.Channel;
+import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import space.space_spring.domain.post.application.port.in.createBoard.CreateBoardCommand;
+import space.space_spring.domain.post.application.port.in.createBoard.CreateBoardUseCase;
+import space.space_spring.domain.post.domain.BoardType;
 import space.space_spring.domain.space.application.port.in.CreateSpaceCommand;
 import space.space_spring.domain.space.application.port.in.CreateSpaceUseCase;
 import space.space_spring.domain.spaceMember.application.port.out.LoadSpaceMemberPort;
@@ -20,7 +25,7 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public class TestTextCommandEventListener extends ListenerAdapter {
     private final LoadSpaceMemberPort loadSpaceMemberPort;
-
+    private final CreateBoardUseCase createBoardUseCase;
     //private final CreateSpaceUseCase createSpaceUseCase;
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event){
@@ -42,6 +47,19 @@ public class TestTextCommandEventListener extends ListenerAdapter {
             loadSpaceMemberPort.loadSpaceMemberBySpaceId(spaceId).stream().forEach(spaceMember->{
                 msg.getChannel().sendMessage("\n"+spaceMember.getNickname()+":"+spaceMember.getId()).queue();
             });
+            return;
+        }
+
+        if(msg.getContentRaw().equals("!createChannel")){
+            MessageChannelUnion channel= event.getChannel();
+            CreateBoardCommand command =CreateBoardCommand.builder()
+                    .boardType(BoardType.POST)
+                    .boardName(channel.getName())
+                    .discordId(channel.getIdLong())
+                    .webhookUrl("https://discordapp.com/api/webhooks/1327265824947310612/9SZPFqGg7fjtYAMvwkTzEtirIMWSKyDegigxupEovyiE_hJgrlOWvt2HO8O3GUM7evoV")
+                    .spaceId(1L)
+                    .build();
+            createBoardUseCase.createBoard(command);
             return;
         }
     }

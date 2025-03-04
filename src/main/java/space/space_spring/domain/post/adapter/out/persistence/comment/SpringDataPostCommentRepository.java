ac9f0c6c06f9f8af35f37cb8412109d35356ec1c
arkchive <kyungminkim7@konkuk.ac.kr>
2025-03-04 17:18:09 +0900

    feat: 게시글 리스트 조회 시, 댓글 개수 조회 로직 추가

---
+++ src/main/java/space/space_spring/domain/post/adapter/out/persistence/comment/SpringDataPostCommentRepository.java
@@ -0,0 +1,17 @@
+package space.space_spring.domain.post.adapter.out.persistence.comment;
+
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+import java.util.List;
+
+public interface SpringDataPostCommentRepository extends JpaRepository<PostCommentJpaEntity, Long>, QuerydslPredicateExecutor<PostCommentJpaEntity> {
+
+    @Query("SELECT new space.space_spring.domain.post.adapter.out.persistence.comment.PostCommentCount(pc.postBase.id, COUNT(pc)) " +
+            "FROM PostCommentJpaEntity pc " +
+            "WHERE pc.postBase.id IN :postIds " +
+            "GROUP BY pc.postBase.id")
+    List<PostCommentCount> countCommentsByPostIds(@Param("postIds") List<Long> postIds);
+}

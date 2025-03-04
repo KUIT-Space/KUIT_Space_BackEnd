40eb2f2f0c6c62b00931f5b67db64e3352cc3055
arkchive <kyungminkim7@konkuk.ac.kr>
2025-03-04 16:36:58 +0900

    feat: 게시글 리스트 조회 시, 좋아요 개수 조회 로직 추가

--- src/main/java/space/space_spring/domain/post/application/service/ReadPostListService.java
+++ src/main/java/space/space_spring/domain/post/application/service/ReadPostListService.java
@@ -6,7 +6,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import space.space_spring.domain.post.application.port.in.readPostList.ListOfPostSummary;
 import space.space_spring.domain.post.application.port.in.readPostList.ReadPostListUseCase;
+import space.space_spring.domain.post.application.port.out.LoadLikePort;
 import space.space_spring.domain.post.application.port.out.LoadPostPort;
+import space.space_spring.domain.post.domain.Post;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -15,9 +20,25 @@ import space.space_spring.domain.post.application.port.out.LoadPostPort;
 public class ReadPostListService implements ReadPostListUseCase {
 
     private final LoadPostPort loadPostPort;
+    private final LoadLikePort loadLikePort;
 
     @Override
     public ListOfPostSummary readPostList(Long boardId) {
-        return loadPostPort.loadPostList(boardId);
+        // 1. Post 도메인 객체 리스트 가져오기
+        List<Post> posts = loadPostPort.loadPostList(boardId);
+
+        // 2. postId 리스트 추출(postId = postBaseId)
+        List<Long> postIds = posts.stream()
+                .map(Post::getId)
+                .toList();
+
+        // 3. 좋아요 수 조회
+        Map<Long, Long> likeCounts = loadLikePort.countLikesByPostIds(postIds);
+
+        // 4. 댓글 수 조회
+        Map<Long, Long> commentCounts = loadCommentPort.countCommentsByPostIds(postIds);
+
+        // 5. ListOfPostSummary 생성
+        return ListOfPostSummary.of();
     }
 }

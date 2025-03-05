bd1ac3bc583bc445b96776d475c6ac74635131b9
arkchive <kyungminkim7@konkuk.ac.kr>
2025-03-05 20:02:45 +0900

    fix: response @Getter 추가

--- src/main/java/space/space_spring/domain/post/adapter/in/web/readPostList/ResponseOfReadPostList.java
+++ src/main/java/space/space_spring/domain/post/adapter/in/web/readPostList/ResponseOfReadPostList.java
@@ -1,11 +1,12 @@
 package space.space_spring.domain.post.adapter.in.web.readPostList;
 
+import lombok.Getter;
 import space.space_spring.domain.post.application.port.in.readPostList.ListOfPostSummary;
 import space.space_spring.domain.post.application.port.in.readPostList.PostSummary;
 
 import java.util.List;
 
-
+@Getter
 public class ResponseOfReadPostList {
 
     private List<ResponseOfPostSummary> readPostList;

986c5c2b73743d7cc0bbc11716e7942a1bbcc3cd
arkchive <kyungminkim7@konkuk.ac.kr>
2025-03-05 15:23:14 +0900

    fix: 게시글 리스트 조회 boardId PathVariable 변경

--- src/main/java/space/space_spring/domain/post/adapter/in/web/readPostList/ReadPostListController.java
+++ src/main/java/space/space_spring/domain/post/adapter/in/web/readPostList/ReadPostListController.java
@@ -2,7 +2,7 @@ package space.space_spring.domain.post.adapter.in.web.readPostList;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import space.space_spring.domain.post.application.port.in.readPostList.ListOfPostSummary;
 import space.space_spring.domain.post.application.port.in.readPostList.ReadPostListUseCase;
@@ -16,7 +16,7 @@ public class ReadPostListController {
     private final ReadPostListUseCase readPostListUseCase;
 
     @GetMapping("/space/{spaceId}/board/{boardId}/post")
-    public BaseResponse<ResponseOfReadPostList> readPostList(@RequestParam Long boardId) {
+    public BaseResponse<ResponseOfReadPostList> readPostList(@PathVariable Long boardId) {
         ListOfPostSummary postSummaries = readPostListUseCase.readPostList(boardId);
         return new BaseResponse<>(ResponseOfReadPostList.of(postSummaries));
     }

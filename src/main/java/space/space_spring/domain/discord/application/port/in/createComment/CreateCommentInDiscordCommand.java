828d03415b64f7538a76b87a3f64bb11cf3be9dc
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2025-03-07 17:17:02 +0900

    [feat] CreateCommentInDiscord Service
    get PostDiscordId not develop yet

--- src/main/java/space/space_spring/domain/discord/application/port/in/createComment/CreateCommentInDiscordCommand.java
+++ src/main/java/space/space_spring/domain/discord/application/port/in/createComment/CreateCommentInDiscordCommand.java
@@ -9,8 +9,10 @@ public class CreateCommentInDiscordCommand {
     private Long spaceId;
     private Long boardId;
     private Long originPostId;
+    //익명일 경우 여기에 디스코드에 보여질 값을 입력
     private String userName;
     private String profileUrl;
+
     private String content;
 
     //private final List<Tag> tags; //not yet

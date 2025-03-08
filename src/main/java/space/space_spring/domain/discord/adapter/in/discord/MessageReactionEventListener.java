8efeeec030b1578f880d49503673d8a5e3308cac
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2025-03-08 23:08:44 +0900

    [feat] discord Add Reaction EventListener
    디스코드에서 이모지를 달면 감지하는 리스너 추가
    아직 usecase 연결 하지 않음

---
+++ src/main/java/space/space_spring/domain/discord/adapter/in/discord/MessageReactionEventListener.java
@@ -0,0 +1,24 @@
+package space.space_spring.domain.discord.adapter.in.discord;
+
+import lombok.RequiredArgsConstructor;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MessageReactionEventListener extends ListenerAdapter {
+    //private final Like
+    @Override
+    public void onMessageReactionAdd(MessageReactionAddEvent event){
+
+        //Todo 게시판 채널에서 들어온 리액션인지 확인
+
+        Long guildId=event.getGuild().getIdLong();
+        Long messageId = event.getMessageIdLong();
+        Long memberId = event.getMember().getIdLong();
+
+        //Todo 좋아요 누르는 UseCase 호출 
+
+    }
+}

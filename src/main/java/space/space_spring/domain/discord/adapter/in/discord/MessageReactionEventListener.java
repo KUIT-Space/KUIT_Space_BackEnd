2b832c52ea3ad21845039200745682282acfec6b
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2025-03-09 00:16:33 +0900

    [feat] add Listen channel check

--- src/main/java/space/space_spring/domain/discord/adapter/in/discord/MessageReactionEventListener.java
+++ src/main/java/space/space_spring/domain/discord/adapter/in/discord/MessageReactionEventListener.java
@@ -1,19 +1,30 @@
 package space.space_spring.domain.discord.adapter.in.discord;
 
 import lombok.RequiredArgsConstructor;
+import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
+import net.dv8tion.jda.api.events.message.GenericMessageEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionRemoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.springframework.stereotype.Component;
+import space.space_spring.domain.post.application.port.out.LoadBoardCachePort;
+
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class MessageReactionEventListener extends ListenerAdapter {
     //private final Like
+    private final DiscordUtil discordUtil;
+    private final LoadBoardCachePort loadBoardCachePort;
     @Override
     public void onMessageReactionAdd(MessageReactionAddEvent event){
 
-        //Todo 게시판 채널에서 들어온 리액션인지 확인
+
+        Optional<Long> boardId = getBoardId(event);
+        if(boardId.isEmpty()){
+            return;
+        }
 
         Long guildId=event.getGuild().getIdLong();
         Long messageId = event.getMessageIdLong();
@@ -25,12 +36,27 @@ public class MessageReactionEventListener extends ListenerAdapter {
 
     @Override
     public void onMessageReactionRemove(MessageReactionRemoveEvent event){
-        //Todo 게시판 채널에서 들어온 리액션인지 확인
+
+
+        Optional<Long> boardId = getBoardId(event);
+        if(boardId.isEmpty()){
+            return;
+        }
 
         Long guildId=event.getGuild().getIdLong();
         Long messageId = event.getMessageIdLong();
         Long memberId = event.getMember().getIdLong();
 
         //Todo 좋아요  삭제하는 UseCase 호출
+        
+    }
+
+    private Optional<Long> getBoardId(GenericMessageEvent event){
+        MessageChannelUnion channel=event.getChannel();
+        Long parentChannelId = discordUtil.getRootChannelId(channel);
+        //log.info("parentChannelId:"+parentChannelId);
+
+        Optional<Long> boardId = loadBoardCachePort.findByDiscordId(parentChannelId);
+
     }
 }

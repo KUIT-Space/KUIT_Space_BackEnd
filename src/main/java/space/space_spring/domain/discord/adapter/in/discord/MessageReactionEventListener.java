1f00f5785fc6b01cd2606d9f65af3b54f66601e5
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2025-03-08 23:11:44 +0900

    [feat] discord reaction remove eventListener
    디스코드 이모지 삭제 감지

--- src/main/java/space/space_spring/domain/discord/adapter/in/discord/MessageReactionEventListener.java
+++ src/main/java/space/space_spring/domain/discord/adapter/in/discord/MessageReactionEventListener.java
@@ -2,6 +2,7 @@ package space.space_spring.domain.discord.adapter.in.discord;
 
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionRemoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.springframework.stereotype.Component;
 
@@ -18,7 +19,18 @@ public class MessageReactionEventListener extends ListenerAdapter {
         Long messageId = event.getMessageIdLong();
         Long memberId = event.getMember().getIdLong();
 
-        //Todo 좋아요 누르는 UseCase 호출 
+        //Todo 좋아요 누르는 UseCase 호출
 
+    }
+
+    @Override
+    public void onMessageReactionRemove(MessageReactionRemoveEvent event){
+        //Todo 게시판 채널에서 들어온 리액션인지 확인
+
+        Long guildId=event.getGuild().getIdLong();
+        Long messageId = event.getMessageIdLong();
+        Long memberId = event.getMember().getIdLong();
+
+        //Todo 좋아요  삭제하는 UseCase 호출
     }
 }

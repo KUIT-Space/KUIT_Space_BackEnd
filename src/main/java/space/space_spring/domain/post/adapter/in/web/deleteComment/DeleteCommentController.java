b9e02022242e23f2d1d1c2aafce5832d3ac3ea33
seongjunnoh <joon1754@naver.com>
2025-03-08 17:41:31 +0900

    feat : 댓글 삭제 api 스웨거 추가

--- src/main/java/space/space_spring/domain/post/adapter/in/web/deleteComment/DeleteCommentController.java
+++ src/main/java/space/space_spring/domain/post/adapter/in/web/deleteComment/DeleteCommentController.java
@@ -1,5 +1,7 @@
 package space.space_spring.domain.post.adapter.in.web.deleteComment;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +14,16 @@ import space.space_spring.global.common.response.SuccessResponse;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Comment", description = "댓글 관련 API")
 public class DeleteCommentController {
 
     private final DeleteCommentUseCase deleteCommentUseCase;
 
+    @Operation(summary = "댓글 삭제", description = """
+            
+            스페이스 멤버가 자신이 생성한 댓글을 삭제합니다.
+            
+            """)
     @DeleteMapping("/space/{spaceId}/board/{boardId}/post/{postId}/comment/{commentId}")
     public BaseResponse<SuccessResponse> deleteComment(@JwtLoginAuth Long spaceMemberId,
                                                        @PathVariable("spaceId") Long spaceId, @PathVariable("boardId") Long boardId, @PathVariable("postId") Long postId, @PathVariable("commentId") Long commentId) {

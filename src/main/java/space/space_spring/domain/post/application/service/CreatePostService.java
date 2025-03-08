636c09069f31be867ee0a5de8f844fe24deb773e
arkchive <kyungminkim7@konkuk.ac.kr>
2025-03-08 21:44:45 +0900

    fix: 게시글 생성 서비스 로직 수정

--- src/main/java/space/space_spring/domain/post/application/service/CreatePostService.java
+++ src/main/java/space/space_spring/domain/post/application/service/CreatePostService.java
@@ -3,18 +3,25 @@ package space.space_spring.domain.post.application.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import space.space_spring.domain.discord.application.port.in.createPost.CreatePostInDiscordCommand;
 import space.space_spring.domain.discord.application.port.in.createPost.CreatePostInDiscordUseCase;
+import space.space_spring.domain.post.application.port.in.createPost.AttachmentInDiscordCommand;
+import space.space_spring.domain.post.application.port.in.createPost.AttachmentOfCreateCommand;
 import space.space_spring.domain.post.application.port.in.createPost.CreatePostCommand;
 import space.space_spring.domain.post.application.port.in.createPost.CreatePostUseCase;
 import space.space_spring.domain.post.application.port.out.CreatePostPort;
 import space.space_spring.domain.post.application.port.out.LoadBoardPort;
-import space.space_spring.domain.post.domain.Board;
-import space.space_spring.domain.post.domain.BoardType;
-import space.space_spring.domain.post.domain.Post;
+import space.space_spring.domain.post.application.port.out.UploadAttachmentPort;
+import space.space_spring.domain.post.domain.*;
 import space.space_spring.domain.spaceMember.application.port.out.LoadSpaceMemberPort;
 import space.space_spring.domain.spaceMember.domian.SpaceMember;
 import space.space_spring.global.exception.CustomException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static space.space_spring.global.common.response.status.BaseExceptionResponseStatus.*;
 
@@ -27,6 +34,7 @@ public class CreatePostService implements CreatePostUseCase {
     private final CreatePostPort createPostPort;
     private final LoadBoardPort loadBoardPort;
     private final LoadSpaceMemberPort loadSpaceMemberPort;
+    private final UploadAttachmentPort uploadAttachmentPort;
     private final CreatePostInDiscordUseCase createPostInDiscordUseCase;
 
     @Override
@@ -41,31 +49,58 @@ public class CreatePostService implements CreatePostUseCase {
         // 3. validate
         validateBoardAndSpaceMember(board, spaceMember, command);
 
+        // 4. S3에 게시글 첨부파일 upload
+        Map<AttachmentType, List<MultipartFile>> attachmentsMap = command.getAttachments().stream()
+                .collect(Collectors.groupingBy(
+                        AttachmentOfCreateCommand::getAttachmentType,
+                        Collectors.mapping(AttachmentOfCreateCommand::getAttachment, Collectors.toUnmodifiableList())
+                ));
+        Map<AttachmentType, List<String>> attachmentUrlsMap = uploadAttachmentPort.uploadAllAttachments(attachmentsMap, "post");
+
         // 4. 디스코드로 게시글 정보 전송
-        Long discordIdForPost = createPostInDiscordUseCase.createPostInDiscord(mapToDiscordCommand(command));
+        List<AttachmentInDiscordCommand> discordAttachments = new ArrayList<>();
+        attachmentUrlsMap.forEach((type, urls) -> urlsToAttachmentCommands(type, urls, discordAttachments));
 
-        // 5. Post 도메인 엔티티 생성 후 Adapter에 저장
+        Long discordIdForPost = createPostInDiscordUseCase.createPostInDiscord(mapToDiscordCommand(command, discordAttachments));
+
+        // 5. Post 도메인 엔티티 생성 후 db에 저장
         Post post = command.toPostDomainEntity(discordIdForPost);
+        Long postId = createPostPort.createPost(post);
 
-        return createPostPort.createPost(post);
+        // 6. Attachment 도메인 엔티티 생성 후 db에 저장
+        List<Attachment> attachments = new ArrayList<>();
+        attachmentUrlsMap.forEach((type, urls) -> urls.forEach(url ->
+                attachments.add(Attachment.withoutId(postId, type, url))
+        ));
+        return postId;
     }
 
     @Override
     @Transactional
     public Long createPostFromDiscord(CreatePostCommand command, Long discordId) {
-        // 1. Post 도메인 엔티티 생성 후 Adapter에 저장
+        // 1. Post 도메인 엔티티 생성 후 db에 저장
         Post post = command.toPostDomainEntity(discordId);
+        Long postId = createPostPort.createPost(post);
 
-        return createPostPort.createPost(post);
+        // 2. Attachment 도메인 엔티티 생성 후 db에 저장
+        /**
+         * TODO : 게시글 생성 Discord Input, attachment 파일 형식 정해진 후 리팩토링
+         */
+        List<Attachment> attachments = new ArrayList<>();
+
+        return postId;
     }
 
 
-    private CreatePostInDiscordCommand mapToDiscordCommand(CreatePostCommand command) {
+    private CreatePostInDiscordCommand mapToDiscordCommand(CreatePostCommand command, List<AttachmentInDiscordCommand> attachments) {
         return CreatePostInDiscordCommand.builder()
-                .postCreatorId(command.getPostCreatorId())
+                .spaceId(command.getSpaceId())
                 .boardId(command.getBoardId())
+                .postCreatorId(command.getPostCreatorId())
                 .title(command.getTitle())
-                .attachments(command.getAttachments())
+                .content(command.getContent())
+                .attachments(attachments)
+                .isAnonymous(command.getIsAnonymous())
                 .build();
     }
 
@@ -85,5 +120,9 @@ public class CreatePostService implements CreatePostUseCase {
             && !spaceMember.isManager()) {
             throw new CustomException(UNAUTHORIZED_USER);
         }
+    }
+
+    private void urlsToAttachmentCommands(AttachmentType attachmentType, List<String> attachmentUrls, List<AttachmentInDiscordCommand> commands) {
+        attachmentUrls.forEach(url -> commands.add(AttachmentInDiscordCommand.create(attachmentType.name(), url)));
     }
 }

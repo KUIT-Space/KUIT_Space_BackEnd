06421950c8610fd8014760973fcf664f24ac92e5
arkchive <kyungminkim7@konkuk.ac.kr>
2025-03-09 20:02:12 +0900

    feat: 게시글 수정 controller 로직 추가 (이미지 수정 x)

---
+++ src/main/java/space/space_spring/domain/post/adapter/in/web/updatePost/UpdatePostController.java
@@ -0,0 +1,60 @@
+package space.space_spring.domain.post.adapter.in.web.updatePost;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import space.space_spring.domain.post.application.port.in.updatePost.UpdatePostCommand;
+import space.space_spring.domain.post.application.port.in.updatePost.UpdatePostUseCase;
+import space.space_spring.global.argumentResolver.jwtLogin.JwtLoginAuth;
+import space.space_spring.global.common.response.BaseResponse;
+import space.space_spring.global.common.response.SuccessResponse;
+import space.space_spring.global.exception.CustomException;
+
+import static space.space_spring.global.common.response.status.BaseExceptionResponseStatus.INVALID_POST_UPDATE;
+import static space.space_spring.global.util.bindingResult.BindingResultUtils.getErrorMessage;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "Post", description = "게시글 관련 API")
+public class UpdatePostController {
+
+    private final UpdatePostUseCase updatePostUseCase;
+
+    @Operation(summary = "게시글 수정", description = """
+            
+            스페이스 멤버가 자신이 생성한 게시글을 수정합니다.
+            
+            """)
+
+    @PutMapping("/space/{spaceId}/board/{boardId}/post/{postId}")
+    public BaseResponse<SuccessResponse> updatePost(@JwtLoginAuth Long spaceMemberId,
+                                                    @PathVariable Long spaceId,
+                                                    @PathVariable Long boardId,
+                                                    @PathVariable Long postId,
+                                                    @Validated @ModelAttribute RequestOfUpdatePost request, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new CustomException(INVALID_POST_UPDATE, getErrorMessage(bindingResult));
+        }
+
+        UpdatePostCommand command = UpdatePostCommand.builder()
+                .spaceId(spaceId)
+                .boardId(boardId)
+                .postId(postId)
+                .postCreatorId(spaceMemberId)
+                .title(request.getTitle())
+                .content(request.getContent())
+                .attachments(request.getAttachments())
+                .isAnonymous(request.getIsAnonymous())
+                .build();
+
+        updatePostUseCase.updatePost(command);
+
+        return new BaseResponse<>(new SuccessResponse(true));
+    }
+}

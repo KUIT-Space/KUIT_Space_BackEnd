2eee7f36dd6a6de1acfdbf71e52ae7f0186c6dbb
arkchive <kyungminkim7@konkuk.ac.kr>
2025-03-11 22:51:34 +0900

    config: swagger 설명 추가

--- src/main/java/space/space_spring/domain/post/adapter/in/web/readBoardList/ReadBoardListController.java
+++ src/main/java/space/space_spring/domain/post/adapter/in/web/readBoardList/ReadBoardListController.java
@@ -1,5 +1,7 @@
 package space.space_spring.domain.post.adapter.in.web.readBoardList;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +13,16 @@ import space.space_spring.global.common.response.BaseResponse;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Board", description = "게시판 관련 API")
 public class ReadBoardListController {
 
     private final ReadBoardListUseCase readBoardListUseCase;
 
+    @Operation(summary = "게시판 목록 조회", description = """
+            
+            사용자가 속한 스페이스 게시판 목록을 조회합니다.
+            
+            """)
     @GetMapping("/space/{spaceId}/board/list")
     public BaseResponse<ResponseOfReadBoardList> readBoardList(
             @JwtLoginAuth Long spaceMemberId,
--- src/main/java/space/space_spring/domain/post/adapter/in/web/readPostList/ReadPostListController.java
+++ src/main/java/space/space_spring/domain/post/adapter/in/web/readPostList/ReadPostListController.java
@@ -1,5 +1,7 @@
 package space.space_spring.domain.post.adapter.in.web.readPostList;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +14,16 @@ import space.space_spring.global.common.response.BaseResponse;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Post", description = "게시글 관련 API")
 public class ReadPostListController {
 
     private final ReadPostListUseCase readPostListUseCase;
 
+    @Operation(summary = "게시글 리스트 조회", description = """
+            
+            스페이스 멤버가 게시판의 게시글 리스트를 조회합니다.
+            
+            """)
     @GetMapping("/space/{spaceId}/board/{boardId}/post")
     public BaseResponse<ResponseOfReadPostList> readPostList(
             @PathVariable Long spaceId,

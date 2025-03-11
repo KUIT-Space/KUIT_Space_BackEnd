0041eb4d20944c320a8fe781533ad1d91f3f926b
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2025-03-11 22:15:38 +0900

    [feat] add Tag table

--- src/main/java/space/space_spring/domain/post/adapter/out/persistence/tag/TagMapper.java
+++ src/main/java/space/space_spring/domain/post/adapter/out/persistence/tag/TagMapper.java
@@ -2,6 +2,8 @@ package space.space_spring.domain.post.adapter.out.persistence.tag;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import space.space_spring.domain.post.adapter.out.persistence.board.BoardJpaEntity;
+import space.space_spring.domain.post.domain.Board;
 import space.space_spring.domain.post.domain.Tag;
 
 @Component
@@ -13,5 +15,13 @@ public class TagMapper {
 
     public Tag toDomainEntity(TagJpaEntity tagJpaEntity) {
         return Tag.create(tagJpaEntity.getId(), tagJpaEntity.getDiscordId(), tagJpaEntity.getTagName(), tagJpaEntity.getBoard().getId());
+    }
+
+    public TagJpaEntity toJpaEntity(Tag tag, BoardJpaEntity boardJpaEntity){
+        return TagJpaEntity.builder()
+                .tagName(tag.getTagName())
+                .discordId(tag.getDiscordId())
+                .board(boardJpaEntity)
+                .build();
     }
 }
--- src/main/java/space/space_spring/domain/post/adapter/out/persistence/tag/TagPersistenceAdapter.java
+++ src/main/java/space/space_spring/domain/post/adapter/out/persistence/tag/TagPersistenceAdapter.java
@@ -1,20 +1,23 @@
 package space.space_spring.domain.post.adapter.out.persistence.tag;
 
-import static space.space_spring.global.common.response.status.BaseExceptionResponseStatus.BOARD_NOT_FOUND;
-import static space.space_spring.global.common.response.status.BaseExceptionResponseStatus.TAG_NOT_FOUND;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import space.space_spring.domain.post.adapter.out.persistence.board.BoardJpaEntity;
 import space.space_spring.domain.post.adapter.out.persistence.board.SpringDataBoardRepository;
+import space.space_spring.domain.post.application.port.out.CreateTagPort;
 import space.space_spring.domain.post.application.port.out.LoadTagPort;
 import space.space_spring.domain.post.domain.Board;
 import space.space_spring.domain.post.domain.Tag;
 import space.space_spring.global.exception.CustomException;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static space.space_spring.global.common.response.status.BaseExceptionResponseStatus.*;
+
 @Repository
 @RequiredArgsConstructor
-public class TagPersistenceAdapter implements LoadTagPort {
+public class TagPersistenceAdapter implements LoadTagPort, CreateTagPort {
 
     private final SpringDataBoardRepository boardRepository;
     private final SpringDataTagRepository springDataTagRepository;
@@ -27,5 +30,30 @@ public class TagPersistenceAdapter implements LoadTagPort {
 
         return springDataTagRepository.findByBoardIdAndTagName(boardJpaEntity, name).map(tagMapper::toDomainEntity)
                 .orElseThrow(() -> new CustomException(TAG_NOT_FOUND));
+    }
+
+
+    public List<Tag> save(List<Tag> tags){
+        if(tags==null||tags.isEmpty()){
+            return List.of();
+        }
+        validateTagOnSameBoard(tags);
+        BoardJpaEntity boardJpaEntity = boardRepository.findById(tags.get(0).getBoardId()).orElseThrow(()-> {
+            throw new CustomException(BOARD_NOT_FOUND);}
+        );
+        List<TagJpaEntity> tagJpaEntities = springDataTagRepository.saveAll(tags.stream().map(tag->tagMapper.toJpaEntity(tag,boardJpaEntity)).toList());
+
+        return tagJpaEntities.stream().map(tagMapper::toDomainEntity).toList();
+    }
+
+    private void validateTagOnSameBoard(List<Tag> tags){
+        if( tags.stream()
+                .map(Tag::getBoardId)
+                .distinct()
+                .limit(2)
+                .count()<=1
+        ){
+            throw new CustomException(TAGS_IS_WORNG);
+        }
     }
 }

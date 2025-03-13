925bea24c18ed8eaeaae909911ac2372e36705f5
arkchive <kyungminkim7@konkuk.ac.kr>
2025-03-13 16:52:59 +0900

    fix: boardId 기준으로 매핑 시, 태그 리스트로 매핑

--- src/main/java/space/space_spring/domain/post/application/service/ReadBoardListService.java
+++ src/main/java/space/space_spring/domain/post/application/service/ReadBoardListService.java
@@ -15,6 +15,7 @@ import space.space_spring.domain.post.domain.Tag;
 import java.util.*;
 import java.util.stream.Collectors;
 
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -37,21 +38,25 @@ public class ReadBoardListService implements ReadBoardListUseCase {
         List<Tag> tagList = loadTagPort.loadTagsByBoardIds(boardIds);
 
         // 3. 태그를 boardId 기준으로 매핑
-        Map<Long, Tag> tagMap = tagList.stream()
-                .collect(Collectors.toMap(Tag::getBoardId, tag -> tag));
+        Map<Long, List<Tag>> tagMap = tagList.stream()
+                .collect(Collectors.groupingBy(Tag::getBoardId));
 
         // 4. 사용자가 구독한 게시판 id 조회
         Set<Long> subscribedBoardIds = new HashSet<>(loadSubscriptionPort.loadSubscribedBoardIds(spaceMemberId));
 
         // 5. 게시판 목록 리스트 생성
         List<ReadBoardInfoCommand> boardInfoCommands = boardList.stream()
-                .map(board -> ReadBoardInfoCommand.of(
-                        board.getId(),
-                        board.getBoardName(),
-                        tagMap.get(board.getId()) != null ? tagMap.get(board.getId()).getId() : null,
-                        tagMap.get(board.getId()) != null ? tagMap.get(board.getId()).getTagName() : null,
-                        subscribedBoardIds.contains(board.getId())
-                )).toList();
+                .flatMap(board -> {
+                    List<Tag> tags = tagMap.getOrDefault(board.getId(), Collections.emptyList());
+                    return tags.stream()
+                            .map(tag -> ReadBoardInfoCommand.of(
+                                    board.getId(),
+                                    board.getBoardName(),
+                                    tag.getId(),
+                                    tag.getTagName(),
+                                    subscribedBoardIds.contains(board.getId())
+                            ));
+                }).toList();
 
         return ReadBoardListCommand.of(boardInfoCommands);
     }

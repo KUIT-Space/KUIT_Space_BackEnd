56e655f23eadbf743a402a50f16ec6760c40907a
hyunn522 <tjgus52213@gmail.com>
2025-03-13 16:11:05 +0900

    feat: 홈 조회 controller 추가

---
+++ src/main/java/space/space_spring/domain/home/adapter/in/web/SubscriptionSummary.java
@@ -0,0 +1,15 @@
+package space.space_spring.domain.home.adapter.in.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubscriptionSummary {
+
+    private String boardName;
+
+    private String boardTitle;
+}

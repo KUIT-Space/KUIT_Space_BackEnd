70f1054f11c31a605af00d277e99f25205d5a75b
hyunn522 <tjgus52213@gmail.com>
2025-03-27 22:35:16 +0900

    fix: 행사 참여자 조회 시 최신순으로 정렬

--- src/main/java/space/space_spring/domain/spaceMember/adapter/out/persistence/custom/SpaceMemberRepositoryCustom.java
+++ src/main/java/space/space_spring/domain/spaceMember/adapter/out/persistence/custom/SpaceMemberRepositoryCustom.java
@@ -1,8 +1,11 @@
 package space.space_spring.domain.spaceMember.adapter.out.persistence.custom;
 
+import java.util.List;
 import java.util.Optional;
 import space.space_spring.domain.spaceMember.domian.SpaceMemberJpaEntity;
 
 public interface SpaceMemberRepositoryCustom {
     Optional<SpaceMemberJpaEntity> findDefaultSpaceMember(Long userId, String defaultSpaceName);
+
+    List<SpaceMemberJpaEntity> findAllByIdInOrder(List<Long> ids);
 }

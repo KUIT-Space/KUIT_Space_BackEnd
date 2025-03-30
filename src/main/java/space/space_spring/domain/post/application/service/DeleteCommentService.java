42f5d5a03fe9ab085015813bcbebe7e89fc0d289
arkchive <kyungminkim7@konkuk.ac.kr>
2025-03-31 02:01:28 +0900

    fix: 댓글 삭제 시, 댓글 좋아요 삭제 처리

--- src/main/java/space/space_spring/domain/post/application/service/DeleteCommentService.java
+++ src/main/java/space/space_spring/domain/post/application/service/DeleteCommentService.java
@@ -11,10 +11,15 @@ import space.space_spring.domain.post.application.port.out.DeleteCommentPort;
 import space.space_spring.domain.post.application.port.out.LoadBoardPort;
 import space.space_spring.domain.post.application.port.out.LoadCommentPort;
 import space.space_spring.domain.post.application.port.out.LoadPostPort;
+import space.space_spring.domain.post.application.port.out.like.DeleteLikePort;
+import space.space_spring.domain.post.application.port.out.like.LoadLikePort;
 import space.space_spring.domain.post.domain.Board;
 import space.space_spring.domain.post.domain.Comment;
+import space.space_spring.domain.post.domain.Like;
 import space.space_spring.domain.post.domain.Post;
 import space.space_spring.global.exception.CustomException;
+
+import java.util.List;
 
 import static space.space_spring.global.common.response.status.BaseExceptionResponseStatus.*;
 import static space.space_spring.global.common.response.status.BaseExceptionResponseStatus.COMMENT_CREATOR_MISMATCH;
@@ -26,7 +31,9 @@ public class DeleteCommentService implements DeleteCommentUseCase {
     private final LoadBoardPort loadBoardPort;
     private final LoadPostPort loadPostPort;
     private final LoadCommentPort loadCommentPort;
+    private final LoadLikePort loadLikePort;
     private final DeleteCommentPort deleteCommentPort;
+    private final DeleteLikePort deleteLikePort;
     private final DeleteCommentInDiscordUseCase deleteCommentInDiscordUseCase;
 
     @Transactional
@@ -36,6 +43,7 @@ public class DeleteCommentService implements DeleteCommentUseCase {
         Board board = loadBoardPort.loadById(command.getBoardId());
         Post post = loadPostPort.loadById(command.getPostId());
         Comment comment = loadCommentPort.loadById(command.getCommentId());
+        List<Like> likes = loadLikePort.loadAllLikes(command.getCommentId());
 
         // 2. validation
         validate(board, post, comment, command);
@@ -48,12 +56,16 @@ public class DeleteCommentService implements DeleteCommentUseCase {
 
         // 4. 댓글 삭제
         deleteCommentPort.deleteComment(command.getCommentId());
+
+        // 5. 댓글 좋아요 삭제
+        deleteLikePort.deleteAllLikes(likes);
     }
 
     @Transactional
     @Override
     public void deleteCommentFromDiscord(Long commentId){
         deleteCommentPort.deleteComment(commentId);
+        // TODO 댓글 좋아요 삭제 처리
     }
 
     private void validate(Board board, Post post, Comment comment, DeleteCommentCommand command) {

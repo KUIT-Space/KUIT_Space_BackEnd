6e5305f00fa4aebb0e3728a8c0347a3c840f93e2
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2025-03-31 21:05:23 +0900

    [feat] message delete in discord

--- src/main/java/space/space_spring/domain/discord/application/port/in/deleteMessageFromDiscordUseCase/DeleteMessageUseCase.java
+++ src/main/java/space/space_spring/domain/discord/application/port/in/deleteMessageFromDiscordUseCase/DeleteMessageUseCase.java
@@ -1,5 +1,6 @@
 package space.space_spring.domain.discord.application.port.in.deleteMessageFromDiscordUseCase;
 
 public interface DeleteMessageUseCase {
-    void delete(Long messageDiscordId);
+    void deleteComment(Long messageDiscordId);
+    void deletePost(Long messageDiscordId);
 }
--- src/main/java/space/space_spring/domain/discord/application/service/DeleteMessageFromDiscordService.java
+++ src/main/java/space/space_spring/domain/discord/application/service/DeleteMessageFromDiscordService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import space.space_spring.domain.discord.application.port.in.deleteMessageFromDiscordUseCase.DeleteMessageUseCase;
 import space.space_spring.domain.post.application.port.in.deleteComment.DeleteCommentUseCase;
+import space.space_spring.domain.post.application.port.in.deletePost.DeletePostUseCase;
 import space.space_spring.domain.post.application.port.out.LoadPostBasePort;
 
 import java.util.Optional;
@@ -13,13 +14,33 @@ import java.util.Optional;
 public class DeleteMessageFromDiscordService implements DeleteMessageUseCase {
     private final LoadPostBasePort loadPostBasePort;
     private final DeleteCommentUseCase deleteCommentUseCase;
+    private final DeletePostUseCase deletePostUseCase;
+
+    /*
+    ToDo 각 삭제 port id 가 통일 안되어 있음
+     */
 
     @Override
-    public void delete(Long messageDiscordId) {
-        Optional<Long> postBaseId = loadPostBasePort.loadByDiscordId(messageDiscordId);
+    public void deleteComment(Long messageDiscordId) {
+        Optional<Long> postBaseId = getPostBaseId(messageDiscordId);
         if (postBaseId.isEmpty()) {
             return;
         }
         deleteCommentUseCase.deleteCommentFromDiscord(postBaseId.get());
+    }
+
+    @Override
+    public void deletePost(Long messageDiscordId){
+        Optional<Long> postBaseId = getPostBaseId(messageDiscordId);
+        if (postBaseId.isEmpty()) {
+            return;
+        }
+
+        deletePostUseCase.deletePostFromDiscord(messageDiscordId);
+    }
+
+    private Optional<Long> getPostBaseId(Long messageDiscordId){
+
+        return loadPostBasePort.loadByDiscordId(messageDiscordId);
     }
 }
